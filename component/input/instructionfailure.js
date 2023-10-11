5f941ec29b7794241e614a0af90784f489e65ede
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-10-11 14:46:32 +0200

    install-next

---
+++ component/input/instructionfailure.js
@@ -0,0 +1,28 @@
+
+// components/instructionfailure.js
+
+import React from 'react';
+
+function Error({ message }) {
+  return (
+    <div className="error">
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default instructionfailure.js;
+// instructionfailure.js
+
+export class instructionfailure {
+    static handleRecipeInstructionError(error) {
+      if (error.message === 'Failed to load instructions') {
+        // Display the error message to the user or take appropriate action
+        console.error('Failed to load instructions. Please try again later.');
+      } else {
+        // Handle other types of errors as needed
+        console.error('An unexpected error occurred:', error);
+      }
+    }
+  }
+  
---
+++ pages/events/[...slug].js
@@ -0,0 +1,4 @@
+import instructionfailure from "@/component/input/instructionfailure";
+
+//export default
+//will wait for the API to be uploaded so that i can export this default
