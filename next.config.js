e14807ed99f2a6654ae206c83af3e0f787bac1ad
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-12 09:05:51 +0200

    env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env:{
+    MONGODB_URI : `mongodb+srv://groupe:lx0hIlKaDof7KBJY@groupe.pse1kuk.mongodb.net/devdb?retryWrites=true&w=majority`,
+  }
 }
 
 module.exports = nextConfig
