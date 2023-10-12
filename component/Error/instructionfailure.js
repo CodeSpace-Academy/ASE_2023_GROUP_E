2050ae78bc27efe7d897d3e5e9b8346420d7719f
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-10-12 17:33:24 +0200

    fixed error message

--- component/Error/instructionfailure.js
+++ component/Error/instructionfailure.js
@@ -1,28 +1,28 @@
 
-// components/instructionfailure.js
+// // components/instructionfailure.js
 
-import React from 'react';
+// import React from 'react';
 
-function Error({ message }) {
-  return (
-    <div className="error">
-      <p>{message}</p>
-    </div>
-  );
-}
+// function Error({ message }) {
+//   return (
+//     <div className="error">
+//       <p>{message}</p>
+//     </div>
+//   );
+// }
 
-export default Error;
-// instructionfailure.js
+// export default Error;
+// // instructionfailure.js
 
-// export class instructionfailure {
-//     static handleRecipeInstructionError(error) {
-//       if (error.message === 'Failed to load instructions') {
-//         // Display the error message to the user or take appropriate action
-//         console.error('Failed to load instructions. Please try again later.');
-//       } else {
-//         // Handle other types of errors as needed
-//         console.error('An unexpected error occurred:', error);
-//       }
-//     }
-//   }
+// // export class instructionfailure {
+// //     static handleRecipeInstructionError(error) {
+// //       if (error.message === 'Failed to load instructions') {
+// //         // Display the error message to the user or take appropriate action
+// //         console.error('Failed to load instructions. Please try again later.');
+// //       } else {
+// //         // Handle other types of errors as needed
+// //         console.error('An unexpected error occurred:', error);
+// //       }
+// //     }
+// //   }
   
--- component/ui/error-alert.js
+++ component/ui/error-alert.js
@@ -1,7 +1,7 @@
-import classes from './error-alert.module.css';
+// import classes from './error-alert.module.css';
 
-function ErrorAlert(props) {
-  return <div className={classes.alert}>{props.children}</div>;
-}
+// function ErrorAlert(props) {
+//   return <div className={classes.alert}>{props.children}</div>;
+// }
 
-export default ErrorAlert;
+// export default ErrorAlert;
