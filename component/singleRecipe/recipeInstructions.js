3c1b2362ed150cd5bb90e500d571fd76ec6792f0
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-12 16:03:25 +0200

    Added mock data of instructions to map over and return an individual recipe instruction. Passed the instruction and index number to the individual instruction. Need to get a better key value instead of the index.

--- component/singleRecipe/recipeInstructions.js
+++ component/singleRecipe/recipeInstructions.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import IndividualRecipeIntruction from './individualRecipeIntruction';
 
-const recipeInstructions = () => {
-  return <div>recipeInstructions</div>;
+const instructions = [
+  'Trim fat from beef, cut into 1-inch pieces.',
+  'In Dutch oven, heat oil over medium high hunt until hot. Add beef (hal…',
+  'Pour off drippings.',
+  'Season with thyme, salt and pepper.',
+  'Stir in broth, wine and garlic. Bring to boil; reduce heat to low.',
+  'Cover tightly and simmer 1 1/2 hours.',
+  'Add carrots and onions.',
+  'Cover and continue cooking 35 to 40 minutes or until beef and vegetabl…',
+  'Bring beef stew to a boil over medium-high heat. Add cornstarch mixtur…',
+  'Reduce heat to medium and cook 3 to 4 minutes or until peas are heated…',
+];
+
+const RecipeInstructions = () => {
+  return (
+    <div>
+      {instructions.map((instruction, index) => {
+        return (
+          <IndividualRecipeIntruction
+            key={index}
+            instruction={instruction}
+            number={index}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
-export default recipeInstructions;
+export default RecipeInstructions;
