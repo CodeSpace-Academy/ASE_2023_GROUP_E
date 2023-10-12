3252803a4786c73a68828a350f7fe91b756af586
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-10-12 17:46:31 +0200

    attempted to fix issue 04

--- component/Error/instructionfailure.js
+++ component/Error/instructionfailure.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import InstructionLoadFailure from '../component/input/instruction-load-failure';
 
 function RecipePage() {
   const [error, setError] = useState(false);
