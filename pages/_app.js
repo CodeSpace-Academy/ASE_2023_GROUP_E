e7c728aec82233702941ce2eb2322393739e3373
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-10-12 17:33:05 +0200

    michelle fixed errors on code

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,5 @@
+import '@/styles/globals.css'
 
-// import { connectDatabase, } from '/database';
-import Error from "next/error";
-
-// Handle database operations
-async function someDatabaseOperation() {
-  const client = await connectDatabase();
-  const collection = 'recipes';
-  const document = { name: 'Your Recipe' };
-
-  try {
-    const insertResult = await insertDocument(client, collection, document);
-    console.log('Inserted document:', insertResult);
-    
-    const sort = { someField: 1 }; // Define your sort criteria
-    const documents = await getAllDocuments(client, collection, sort);
-    console.log('All documents:', documents);
-  } catch (error) {
-    InstructionFailure.handleRecipeInstructionError(error);
-  } finally {
-    client.close(); // Don't forget to close the database connection.
-  }
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-// Render your UI components
-function App() {
-  return (
-    <div>
-      <Error message="An error occurred" />
-      <button onClick={someDatabaseOperation}>Perform Database Operation</button>
-    </div>
-  );
-}
-
-export default App
