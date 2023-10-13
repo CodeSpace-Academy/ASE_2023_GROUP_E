aca92defdd119354f34889b759322ce99eabbd66
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-13 15:23:48 +0200

    Dynamic single recipe page can uses get static props to pregenerates the page and passes the single recipe to the single recipe page component. Define paths to be pregenerate in getStaticPaths. Need to create text for undefined paths.

--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { run } from '@/database';
+import TagsContainer from '@/component/recipe-tags/TagsContainer';
 
-const singleRecipePage = () => {
-  return <div>singleRecipePage</div>;
+const singleRecipePage = ({ singleRecipe, allRecipes }) => {
+  const {
+    category,
+    cook,
+    description,
+    images,
+    ingredients,
+    instructions,
+    nutrition,
+    prep,
+    published,
+    servings,
+    tags,
+    title,
+    _id: id,
+  } = singleRecipe;
+
+  return <div>{title} </div>;
 };
-
 export default singleRecipePage;
+
+export const getStaticProps = async ({ params }) => {
+  const allRecipes = await run();
+  const recipeId = params.recipeId;
+  const singleRecipe = allRecipes.find((recipe) => {
+    return recipe._id === recipeId;
+  });
+
+  // return { props: { allRecipes } };
+  return { props: { singleRecipe } };
+};
+export const getStaticPaths = async () => {
+  const allRecipes = await run();
+  const allPaths = allRecipes.map((recipe) => {
+    const id = recipe._id;
+    return { params: { recipeId: `${id}` } };
+  });
+  return {
+    paths: allPaths,
+
+    fallback: false, // to create text for if it's a path not included
+  };
+};
