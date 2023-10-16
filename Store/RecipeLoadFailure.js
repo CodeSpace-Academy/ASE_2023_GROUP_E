bea15f45d31e40aa04dbd18af28ba851b9acbe57
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-10-16 16:37:54 +0200

    commented out all recipes folder

--- Store/RecipeLoadFailure.js
+++ Store/RecipeLoadFailure.js
@@ -1,19 +1,49 @@
-const NotificationContext = createContext ({
-    notification : null,
-    showNotification: function () {},
-    hideNotification: function () {}
+import { createContext, useState, useEffect } from 'react';
 
+const NotificationContext = createContext({
+  notification: null, // { title, message, status }
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
+});
 
-})
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
 
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
 
-export function NotificationContextProvider (props) {
-    <NotificationContext.Provider>
-    {props.children}
-   </NotificationContext.Provider>
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
 }
 
-
-
-
-export default NotificationContext
+export default NotificationContext;
--- component/Recipes/all-recipes.js
+++ component/Recipes/all-recipes.js
@@ -1,61 +1,61 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
-import NumToTime from '../handlerTime/timeRead';
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
+// import Grid from '@mui/material/Unstable_Grid2';
+// import NumToTime from '../handlerTime/timeRead';
 
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  boxShadow:'none',
-  borderBottom: 'solid black 2px',
-  borderRight: 'solid black 2px',
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(2),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+//   boxShadow:'none',
+//   borderBottom: 'solid black 2px',
+//   borderRight: 'solid black 2px',
+// }));
 
-export default function Recipes({recipes, click}) {
-  return (
-    <>
+// export default function Recipes({recipes, click}) {
+//   return (
+//     <>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={1}>
+//       <Box sx={{ flexGrow: 1 }}>
+//         <Grid container spacing={1}>
 
-          {
-            recipes && recipes.map((recipe) => {
-              return(
-                <Grid xs={12} md={4} key={recipe.id}>
-                <Item>
-                  <h4>{recipe.title}</h4>
-                  <p>{recipe.description}</p>
+//           {
+//             recipes && recipes.map((recipe) => {
+//               return(
+//                 <Grid xs={12} md={4} key={recipe.id}>
+//                 <Item>
+//                   <h4>{recipe.title}</h4>
+//                   <p>{recipe.description}</p>
                   
-                  {/* adding time to display on preview */}
-                 <p>⏲️Prep:{NumToTime(recipe.prep)}</p>
-                  <p>🕰️Cook:{NumToTime (recipe.cook)}</p>
+//                   {/* adding time to display on preview */}
+//                  <p>⏲️Prep:{NumToTime(recipe.prep)}</p>
+//                   <p>🕰️Cook:{NumToTime (recipe.cook)}</p>
 
 
-                  {/* here is the total time for (added prep and cook) */}
-                 <p>⏰Total Time: {NumToTime(recipe.prep + recipe.cook)}</p>
+//                   {/* here is the total time for (added prep and cook) */}
+//                  <p>⏰Total Time: {NumToTime(recipe.prep + recipe.cook)}</p>
         
-                  {/* <Image src={recipe.images[0]} alt='image' width={200} height={200}/> */}
-                  <ol>{recipe.tags.map((m) => <li>{m}</li>)}</ol >
+//                   {/* <Image src={recipe.images[0]} alt='image' width={200} height={200}/> */}
+//                   <ol>{recipe.tags.map((m) => <li>{m}</li>)}</ol >
                 
                 
-                  <button onClick={recipe.description}>Details</button>
-                </Item>
+//                   <button onClick={recipe.description}>Details</button>
+//                 </Item>
                 
-              </Grid>
-              )
-            })
-          }
+//               </Grid>
+//               )
+//             })
+//           }
 
-        </Grid>
-      </Box>
+//         </Grid>
+//       </Box>
 
-      <button onClick={click} >Load More </button>
-    </>
-  );
-}
+//       <button onClick={click} >Load More </button>
+//     </>
+//   );
+// }
