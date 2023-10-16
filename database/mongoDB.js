9dd6323f8a9aba8cda1bee336460abc558727392
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-16 10:05:42 +0200

    deleted unused files

--- database/mongoDB.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-export async function connectDatabase() {
-
-  const client = await MongoClient.connect('mongodb+srv://groupe:lx0hIlKaDof7KBJY@groupe.pse1kuk.mongodb.net/devdb?retryWrites=true&w=majority')
-
-  return client
-
-}
