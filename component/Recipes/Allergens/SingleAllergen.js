9530a29511b4e67644a699d32c05719b874448ad
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-18 22:02:31 +0200

    Added comments

--- component/Recipes/Allergens/SingleAllergen.js
+++ component/Recipes/Allergens/SingleAllergen.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import classes from './SingleAllergen.module.css';
+/**
+ * Takes in and displays an allergen string
+ * @param {string} allergen
+ * @returns
+ */
 const SingleAllergen = ({ allergen }) => {
   return <li className={classes.listItem}>{allergen}</li>;
 };
--- component/Recipes/Allergens/SingleRecipeAllergens.js
+++ component/Recipes/Allergens/SingleRecipeAllergens.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import SingleAllergen from './SingleAllergen';
 import classes from './SingleRecipeAllergens.module.css';
+/**
+ * Expects an array of allergens as props and maps over the array to display individual allergen items
+ * @param {array} allergenList
+ * @returns
+ */
 const SingleRecipeAllergens = ({ allergensList }) => {
   return (
     <ul className={classes.list}>
