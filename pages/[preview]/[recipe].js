e896dec351f49ed144edfab0657160511c04eaeb
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-18 19:32:34 +0200

    Fetched the allergens array in getServerSideProps. Passed the array as a prop into the RecipePage componentsa and passed it down to the Recipe Details component.

--- pages/[preview]/[recipe].js
+++ pages/[preview]/[recipe].js
@@ -3,21 +3,29 @@
 import React from 'react';
 import { run } from '@/database';
 import RecipeDetails from '@/component/Recipes/Details/RecipeDetails';
-
-const RecipePage = ({ recipe }) => {
-  return <RecipeDetails recipe={recipe} />;
+import { fetchAllergensFromDatabase } from '@/database/allergensData';
+const RecipePage = ({ recipe, allergens }) => {
+  console.log(allergens);
+  return <RecipeDetails recipe={recipe} allergens={allergens} />;
 };
 
 export async function getServerSideProps({ params }) {
   const { preview, recipe } = params;
 
   // Fetch the recipe details based on preview and recipe params
-  const {documents} = await run(parseInt(preview));
+  const { documents } = await run(parseInt(preview));
+  const { allergens } = await fetchAllergensFromDatabase();
   const selectedRecipe = documents.find((r) => r.title === recipe);
 
+  // // Convert the _id fields of allergens to strings
+  // const allergens = allergensDocuments.map((allergen) => ({
+  //   ...allergen,
+  //   _id: allergen._id.toString(),
+  // }));
   return {
     props: {
       recipe: selectedRecipe || null, // Pass the selected recipe as props
+      allergens: allergens,
     },
   };
 }
