75f5307d9a4f75b0bd59a97f8bb1c408c9228be0
Kelello-7206 <tseripe7@gmail.com>
2023-10-18 16:16:04 +0200

    commit

--- component/Recipes/Details/RecipeDetails.js
+++ component/Recipes/Details/RecipeDetails.js
@@ -1,10 +1,10 @@
 // component/Recipe/Preview/RecipeDetails
 import React from 'react';
-import Image from 'next/image';
 import styles from './recipeDetails.module.css';
 import NumToTime from '@/component/handlerTime/timeRead';
 import ImageSlider from './ImageSlider';
 import SingleRecipeTags from '../SingleRecipeTags/SingleRecipeTags';
+
 const RecipeDetails = ({ recipe }) => {
   if (!recipe) return null;
 
@@ -23,54 +23,42 @@ const RecipeDetails = ({ recipe }) => {
         <p>
           <strong>Category:</strong> {recipe.category}
         </p>
-        <p>
+        <div>
           <strong>Tags:</strong> <SingleRecipeTags tags={recipe.tags} />
-        </p>
-      </div>
-      <div className={styles.nutrition}>
-        <h2>Nutrition:</h2>
-        <div className={styles.listContainer}>
-          <ul>
-            {recipe.nutrition &&
-              Object.entries(recipe.nutrition).map(([key, value]) => (
-                <li key={key}>{`${key}: ${value}`}</li>
-              ))}
-          </ul>
         </div>
-      </div>
-      <div className={styles.ingredients}>
-        <h2>Ingredients:</h2>
-        <div className={styles.listContainer}>
-          <ul>
-            {recipe.ingredients &&
-              Object.entries(recipe.ingredients).map(([ingredient, amount]) => (
-                <li key={ingredient}>{`${ingredient}: ${amount}`}</li>
-              ))}
-          </ul>
+        <div>
+          ⏲️ Prep: {NumToTime(recipe.prep)}<br />
+          🕰️ Cook: {NumToTime(recipe.cook)}<br />
+          ⏰ Total Time: {NumToTime(recipe.prep + recipe.cook)}
         </div>
-      </div>
-      {/* Add prep time here */}
-
-      <div>
-        <p>{recipe.description.substring(0, 170)}</p>
-      </div>
-      <div>
-        {/*adding time to display on preview */}
-        ⏲️ Prep: {NumToTime(recipe.prep)}
-      </div>
-      <div>🕰️ Cook: {NumToTime(recipe.cook)}</div>
-      {/* total time for (added prep and cook) */}
-      <div>⏰ Total Time: {NumToTime(recipe.prep + recipe.cook)}</div>
-
-      <div className={styles.instructions}>
-        <h2>Instructions:</h2>
-        <div className={styles.listContainer}>
-          <ol>
-            {recipe.instructions &&
-              recipe.instructions.map((instruction, index) => (
-                <li key={index}>{instruction}</li>
-              ))}
-          </ol>
+        <div className={styles.flexContainer}>
+          <div className={styles.columnItem}>
+            <h2>Nutrition</h2>
+            <ul>
+              {recipe.nutrition &&
+                Object.entries(recipe.nutrition).map(([key, value]) => (
+                  <li key={key}>{`${key}: ${value}`}</li>
+                ))}
+            </ul>
+          </div>
+          <div className={styles.columnItem}>
+            <h2>Ingredients</h2>
+            <ul>
+              {recipe.ingredients &&
+                Object.entries(recipe.ingredients).map(([ingredient, amount]) => (
+                  <li key={ingredient}>{`${ingredient}: ${amount}`}</li>
+                ))}
+            </ul>
+          </div>
+          <div className={styles.columnItem}>
+            <h2>Instructions</h2>
+            <ol>
+              {recipe.instructions &&
+                recipe.instructions.map((instruction, index) => (
+                  <li key={index}>{instruction}</li>
+                ))}
+            </ol>
+          </div>
         </div>
       </div>
     </div>
