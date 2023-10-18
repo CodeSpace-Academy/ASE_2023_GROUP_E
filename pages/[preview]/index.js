bd0849e976fc8856bcbf288de5e4da07ded820da
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-10-18 11:31:25 +0200

    Changes

--- pages/[preview]/index.js
+++ pages/[preview]/index.js
@@ -1,44 +1,70 @@
 import PreviewList from '@/component/Recipes/Preview/PreviewList';
 import { run } from '@/database';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 
 export default function AllRecipes({ documents, pagesPath, totalDataLength }) {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(pagesPath);
   const recipesToLoad = 100;
-  console.log(documents)
-
-  // Calculate the initial value for Load More
-  const initialLoadMoreValue = totalDataLength - currentPage;
-  const [loadMoreValue, setLoadMoreValue] = useState(initialLoadMoreValue);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loadMoreValue, setLoadMoreValue] = useState(totalDataLength - currentPage);
+  const [loading, setLoading] = useState(false); // Initialize loading state
 
   const loadMoreRecipes = () => {
+    // Prevent multiple clicks while loading
+    if (loading) return;
+
+    // Set loading state to true
+    setLoading(true);
+
     // Calculate the number of recipes to load for the next page
     const nextPage = currentPage + recipesToLoad;
     setCurrentPage(nextPage);
 
-  // Calculate the new value based on the updated currentPage and recipesToLoad
-    const newLoadMoreValue = initialLoadMoreValue - recipesToLoad;
-setLoadMoreValue(newLoadMoreValue);
+    // Calculate the new value based on the updated currentPage and recipesToLoad
+    const newLoadMoreValue = totalDataLength - nextPage;
+    setLoadMoreValue(newLoadMoreValue);
+
+    // Increment the page number
+    setPageNumber(pageNumber + 1);
 
     // Navigate to the next page with the appropriate number of recipes to load
     router.push(`/${nextPage}`);
   };
-  
+
+  const goBack = () => {
+    // Decrease the page number
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+
+      // Increase the load more value when going back
+      const prevPage = currentPage - recipesToLoad;
+      setLoadMoreValue(totalDataLength - prevPage);
+
+      // Set loading state to true
+      setLoading(true);
+
+      // Navigate back to the previous page
+      router.push(`/${prevPage}`);
+    }
+  };
+
+  // Add a useEffect to reset loading state
+  useEffect(() => {
+    setLoading(false);
+  }, [currentPage]);
+
   return (
     <main>
-      {/* <Recipes
-        recipes={results && results}
-        click={loadMoreRecipes}
-      /> */}
-
-      <PreviewList 
-        recipes={documents && documents}
-        click={loadMoreRecipes}
-      />
-    <button onClick={loadMoreRecipes}>Load More {initialLoadMoreValue}</button>
+      <PreviewList recipes={documents} click={loadMoreRecipes} />
+      <button onClick={loadMoreRecipes} disabled={loading}>
+        {loading ? 'Loading...' : `Load More ${loadMoreValue}`}
+      </button>
+      <button onClick={goBack} disabled={loading}>
+        Go Back
+      </button>
+      <button>{pageNumber}</button>
     </main>
   );
 }
@@ -46,7 +72,7 @@ setLoadMoreValue(newLoadMoreValue);
 export async function getServerSideProps({ params }) {
   const { preview } = params;
   const pagesPath = parseInt(preview);
-  const {documents, totalDataLength} = await run(pagesPath);
+  const { documents, totalDataLength } = await run(pagesPath);
 
   return {
     props: {
@@ -56,3 +82,4 @@ export async function getServerSideProps({ params }) {
     },
   };
 }
+
