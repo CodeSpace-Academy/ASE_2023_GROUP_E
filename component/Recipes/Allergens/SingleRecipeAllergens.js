99daa44ad12de8074c5a3f2bddab0cedd99f3d73
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-18 21:56:10 +0200

    The component accepts an allergensList as props and loops through it to return single allergens as list items. Pass in the allergen strings to the single allergen component

--- component/Recipes/Allergens/SingleRecipeAllergens.js
+++ component/Recipes/Allergens/SingleRecipeAllergens.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
-const SingleRecipeAllergens = () => {
-  return <div>SingleRecipeAllergens</div>;
+import SingleAllergen from './SingleAllergen';
+import classes from './SingleRecipeAllergens.module.css';
+const SingleRecipeAllergens = ({ allergensList }) => {
+  return (
+    <ul className={classes.list}>
+      {allergensList.map((allergen, index) => {
+        return <SingleAllergen key={index} allergen={allergen} />;
+      })}
+    </ul>
+  );
 };
 
 export default SingleRecipeAllergens;
