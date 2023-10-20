fd124eb6b656c202063cde6d78942d94a909f759
Otsile076 <otsilekatane@gmail.com>
2023-10-20 10:55:47 +0200

    update description

--- component/Recipes/Details/RecipeDetails.js
+++ component/Recipes/Details/RecipeDetails.js
@@ -8,10 +8,14 @@ import SingleRecipeTags from '../SingleRecipeTags/SingleRecipeTags';
 import ErrorMessage from '@/component/Error/ErrorMessage';
 import IndividualRecipeIntruction from '@/component/singleRecipe/instructions/individualRecipeIntruction';
 import SingleRecipeAllergens from '../Allergens/SingleRecipeAllergens';
+import EditDescription from '@/component/editDecription/editDescription';
+import StateContext from '@/useContext/StateContext';
+import Button from '@/component/Button/button';
 
 const RecipeDetails = ({ recipe, allergens }) => {
   const [showIngredients, setShowIngredients] = useState(false);
   const [showNutrition, setShowNutrition] = useState(false);
+  const { edit, setEdit }= StateContext()
 
   /**
    * Contains the allergens present in this recipe
@@ -40,6 +44,12 @@ const RecipeDetails = ({ recipe, allergens }) => {
         <div className={styles.info}>
           <div className={styles.info1}> 
           <div><strong>Description:</strong> {recipe.description}</div>
+
+          {/**
+           * {@link info} is a props that hold the current descripton that will get modified
+           */}
+          {  edit ? <EditDescription info={recipe.description} /> : <Button color={'success'} text={'Edit Description'} click={() => setEdit(!edit)}/>}
+
           <div><strong>Category:</strong> {recipe.category}</div>
           <div> <SingleRecipeTags tags={recipe.tags} /></div>
           <div className={styles.aligned}>
