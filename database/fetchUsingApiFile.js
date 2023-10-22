0870e04a1a7d68cb6c4c7d0604ded83982f53647
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-22 06:54:46 +0200

    jsdoc in the fetchUsingApiFile

--- database/fetchUsingApiFile.js
+++ database/fetchUsingApiFile.js
@@ -1,7 +1,11 @@
+
 /**
+ * Asynchronously adds an item to a specified API endpoint using a POST request.
  *
- * @param {object} item is an object that hold is used in the body
- * {@link addItem} is used to connect the api folder,  
+ * @param {string} apiPath - The URL or path of the API endpoint where the item will be added. eg('/api/filename')
+ * @param {Object} item - The item to be added to the API. Should be a JavaScript object.eg({key: value})
+ * @returns {Promise<Object>} A promise that resolves to the response data from the API.
+ * @throws {Error} If the POST request fails or the response status is not OK, an error is thrown with a message.
  */
 export async function addItem(apiPath, item) {
     const response = await fetch(apiPath, {
@@ -12,15 +16,11 @@ export async function addItem(apiPath, item) {
       },
     });
   
-  
     const data = await response.json();
-  
-  
+
     if (!response.ok) {
       throw new Error(data.message || 'Something went wrong!');
     }
-  
-  
     return data;
   }
   
