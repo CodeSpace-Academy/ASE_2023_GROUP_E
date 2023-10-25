54906c9a72a90fe892dadfa683aac0a7c2c56f70
Kelello-7206 <tseripe7@gmail.com>
2023-10-25 14:26:02 +0200

    deleted css and modified component

--- component/Filter/ingredients.js
+++ component/Filter/ingredients.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import styles from './IngredientsList.module.css';
+import React, { useState } from 'react';
 
 const IngredientsList = ({ recipes }) => {
-  return (
-    <div className={styles.ingredientList}>
-      {recipes &&
-        recipes.map((ingredientObj, index) => {
-          // Extract the actual ingredient string from the object
-          const ingredient = Object.keys(ingredientObj)[0];
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+  
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-          return (
-            <div className={styles.item} key={index}>
-              <button>{ingredient}</button>
-            </div>
-          );
-        })}
+  const toggleItem = (item) => {
+    if (selectedItems.includes(item)) {
+      setSelectedItems(selectedItems.filter((i) => i !== item));
+    } else {
+      setSelectedItems([...selectedItems, item]);
+    }
+  };
+
+  return (
+    <div className={styles.dropdownChecklist}>
+      <div className={styles.dropdownToggle} onClick={toggleDropdown}>
+        Toggle Dropdown
+      </div>
+      {isOpen && (
+        <div className={styles.dropdownMenu}>
+          {recipes &&
+            recipes.map((ingredientObj, index) => {
+              const ingredient = Object.keys(ingredientObj)[0];
+              return (
+                <div className={styles.item} key={index}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={selectedItems.includes(ingredient)}
+                      onChange={() => toggleItem(ingredient)}
+                    />
+                    {ingredient}
+                  </label>
+                </div>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 };
