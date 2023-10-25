adbfd8d2cb6409c6284378dac8afc8826d479987
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-25 13:26:30 +0200

    Added isInFavourites state to the favourites button and can change the state using the toggleIsInFavourites button.

--- component/Recipes/FavouritesButton/FavouritesButton.js
+++ component/Recipes/FavouritesButton/FavouritesButton.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './FavouritesButton.module.css';
-const FavouritesButton = ({ onClick }) => {
+const FavouritesButton = ({ onClick, isFavourited }) => {
+  const [isInFavourites, setIsInFavourites] = useState(isFavourited);
+
+  const toggleIsFavourite = () => {
+    setIsInFavourites((prevIsInFavourite) => !isInFavourites);
+  };
   return (
     <div>
-      <button onClick={onClick} className={classes.favButton}>
+      <button
+        onClick={toggleIsFavourite}
+        className={
+          isInFavourites
+            ? `${classes.isFavourite} ${classes.favButton}`
+            : classes.favButton
+        }
+      >
         Add to favourites
       </button>
     </div>
