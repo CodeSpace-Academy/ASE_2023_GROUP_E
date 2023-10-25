91318acfd1fed184e1a4e201f9cb483086adb0ee
Kelello-7206 <tseripe7@gmail.com>
2023-10-25 09:54:57 +0200

    added css and edited component

---
+++ component/Filter/ingredients.js
@@ -0,0 +1,23 @@
+// component/Filter/ingredients
+import React from 'react';
+import styles from './IngredientsList.module.css';
+
+const IngredientsList = ({ recipes }) => {
+  return (
+    <div className={styles.ingredientList}>
+      {recipes &&
+        recipes.map((ingredientObj, index) => {
+          // Extract the actual ingredient string from the object
+          const ingredient = Object.keys(ingredientObj)[0];
+
+          return (
+            <div className={styles.item} key={index}>
+              <button>{ingredient}</button>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default IngredientsList;
--- pages/filterData/index.js
+++ pages/filterData/index.js
@@ -1,17 +1,44 @@
 // pages/filterData/index.js
-import IngredientsFilter from '@/component/Filter/ingredientsFilter';
-import { fetchIngredients } from '@/database/filterData';
 
-const FilterData = ({ ingredients }) => {
+import React, { useEffect, useState } from 'react';
+import { run } from '@/database';
+import IngredientsList from '@/component/Filter/ingredients';
+
+export default function IngredientList({ documents }) {
+  const [uniqueIngredient, setUniqueIngredient] = useState([]);
+ 
+  useEffect(() => {
+    if (documents) {
+      const  AllIngredients = documents.reduce((ingredients, recipe) => {
+        return ingredients.concat(recipe.ingredients);
+      }, []);
+
+      const uniqueIngredient = [...new Set(AllIngredients)]; // Remove duplicates
+      setUniqueIngredient(uniqueIngredient);
+    }
+  }, [documents]);
+
   return (
     <div>
-      <IngredientsFilter ingredients={ingredients} />
+      <IngredientsList recipes={uniqueIngredient} />
     </div>
   );
-};
-
-export default FilterData;
+}
 
 export async function getStaticProps() {
-  
+  try {
+    const {documents} = await run();
+
+    return {
+      props: {
+        documents,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: 'Failed to fetch data. Please check your network connection.',
+      },
+    };
+  }
 }
