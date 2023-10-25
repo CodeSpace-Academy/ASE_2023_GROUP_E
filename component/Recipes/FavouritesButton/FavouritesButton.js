b89290f6bedb0eabf21f8f725b1237d8a9e4ef7b
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-25 22:25:45 +0200

    Removed isInFavourites state

--- component/Recipes/FavouritesButton/FavouritesButton.js
+++ component/Recipes/FavouritesButton/FavouritesButton.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import classes from './FavouritesButton.module.css';
 const FavouritesButton = ({ recipe }) => {
-  const [isInFavourites, setIsInFavourites] = useState(null);
   const [favouritesList, setFavouritesList] = useState(
     JSON.parse(localStorage.getItem('favs')) || []
   );
@@ -11,19 +10,17 @@ const FavouritesButton = ({ recipe }) => {
 
   useEffect(() => {
     localStorage.setItem('favs', JSON.stringify(favouritesList));
-  }, [favouritesList, isInFavourites]);
+  }, [favouritesList]);
 
   const toggleIsInFavourites = () => {
     if (!recipeIsInFavouritesList) {
       setFavouritesList((prevFavourites) => [...prevFavourites, recipe]);
-      setIsInFavourites(true);
     } else {
       setFavouritesList((prevFavourites) =>
         prevFavourites.filter((singleRecipe) => {
           return singleRecipe._id !== recipe._id;
         })
       );
-      setIsInFavourites(false);
     }
   };
   return (
