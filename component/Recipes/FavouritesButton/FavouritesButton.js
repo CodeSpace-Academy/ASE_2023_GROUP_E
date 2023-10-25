8751d4f214807fa9074d8a45bc83baf5ff50d225
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-25 21:55:20 +0200

    Added favouritesList state. ToggleIsInFavourites now adds the recipe to the favouritesList when the button is click (only if it's not already present in the list)

--- component/Recipes/FavouritesButton/FavouritesButton.js
+++ component/Recipes/FavouritesButton/FavouritesButton.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './FavouritesButton.module.css';
-const FavouritesButton = ({ onClick, isFavourited }) => {
+const FavouritesButton = ({ onClick, isFavourited, recipe }) => {
   const [isInFavourites, setIsInFavourites] = useState(isFavourited);
-
+  const [favouritesList, setFavouritesList] = useState(
+    JSON.parse(localStorage.getItem('favs')) || []
+  );
+  console.log(favouritesList);
+  useEffect(() => {
+    localStorage.setItem('favs', JSON.stringify(favouritesList));
+  }, [favouritesList]);
   const toggleIsInFavourites = () => {
-    setIsInFavourites((prevIsInFavourite) => !isInFavourites);
+    // setIsInFavourites((prevIsInFavourite) => !isInFavourites);
+    const recipeIsInFavouritesList = favouritesList.find((singleRecipe) => {
+      return singleRecipe._id === recipe._id;
+    });
+    console.log(recipeIsInFavouritesList);
+    if (!recipeIsInFavouritesList) {
+      setFavouritesList((prevFavourites) => [...prevFavourites, recipe]);
+    }
   };
   return (
     <div>
