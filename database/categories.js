ccbf391461d2501474f453517c8b739589fb8e48
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-10-26 09:51:32 +0200

    removed let client in categories page

--- database/categories.js
+++ database/categories.js
@@ -1,40 +1,40 @@
-import{client} from '.index'
+// import{client} from '.index'
 
-export async function fetchCategories() {
+// export async function fetchCategories() {
 
 
-  try {
-    // const uri = process.env.MONGODB_URI;
+//   try {
+//     // const uri = process.env.MONGODB_URI;
 
-    if (!uri) {
-      console.error('failed to connect');
-      return {
-        categories: [],
-      };
-    }
+//     if (!uri) {
+//       console.error('failed to connect');
+//       return {
+//         categories: [],
+//       };
+//     }
 
  
 
-    // await client.connect();
-    // const db = client.db('devdb');
+//     // await client.connect();
+//     // const db = client.db('devdb');
 
-    const categoriesCollection = db.collection('categories');
-    const categoriesDocuments = await categoriesCollection.find({}).toArray();
-    console.log('categoriesDocuments:', categoriesDocuments);
-    const categories = categoriesDocuments.map((doc) => doc.name);
-    console.log('categories:', categories);
+//     const categoriesCollection = db.collection('categories');
+//     const categoriesDocuments = await categoriesCollection.find({}).toArray();
+//     console.log('categoriesDocuments:', categoriesDocuments);
+//     const categories = categoriesDocuments.map((doc) => doc.name);
+//     console.log('categories:', categories);
 
-    return {
-      categories,
-    };
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    return {
-      categories: [],
-    };
-  } finally {
-    if (client) {
-      await client.close();
-    }
-  }
-}
+//     return {
+//       categories,
+//     };
+//   } catch (error) {
+//     console.error('Error connecting to the database:', error);
+//     return {
+//       categories: [],
+//     };
+//   } finally {
+//     if (client) {
+//       await client.close();
+//     }
+//   }
+// }
--- pages/api/recipesbyCat/[category].js
+++ pages/api/recipesbyCat/[category].js
@@ -1,14 +1,14 @@
-import Recipe from '../../../models/Recipe';
+// import Recipe from '../../../models/Recipe';
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const categories = await Recipe.distinct('category'); // Assuming 'category' is the field in your Recipe model
-      res.status(200).json(categories);
-    } catch (error) {
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
+// export default async function handler(req, res) {
+//   if (req.method === 'GET') {
+//     try {
+//       const categories = await Recipe.distinct('category'); // Assuming 'category' is the field in your Recipe model
+//       res.status(200).json(categories);
+//     } catch (error) {
+//       res.status(500).json({ message: 'Internal Server Error' });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Method not allowed' });
+//   }
+// }
