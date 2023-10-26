422eb296eb27c696d8abcfbae4fbf0be3f4f69f9
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-10-26 16:31:10 +0200

    Renamed page to dynamic page

---
+++ pages/categories/[categoriesPage].js
@@ -0,0 +1,27 @@
+// pages/search.js
+// import SearchBar from '../components/SearchBar';
+import getFilteredRecipes from '@/database/getFiltedRecipes';
+import PreviewList from '@/component/Recipes/Preview/PreviewList';
+
+const Search = ({Result}) => {
+
+  return (
+    <div>
+      <h1>Recipe Search</h1>
+      {/* <SearchBar /> */}
+      <PreviewList recipes={Result} />
+    </div>
+  );
+};
+
+export async function getServerSideProps(context){
+  const SearchWord = context.params.categoriesPage
+  const Result =  await getFilteredRecipes(0, {category: SearchWord})
+  return {
+   props:{
+    Result,
+   },
+  }
+}
+
+export default Search;
