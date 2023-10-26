9c931da05dbddb8af534983bbe48cc3b67171c08
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-10-26 13:56:43 +0200

    added a Api for categories

---
+++ pages/api/categories.js
@@ -0,0 +1,11 @@
+// pages/api/recipes.js
+export default async function handler(req, res) {
+    const category = req.query.category;
+  
+    // Perform database query to fetch recipes based on category
+    // Example using a hypothetical function `fetchRecipesFromDatabase(category)`
+    const recipes = await fetchRecipesFromDatabase(category);
+  
+    res.status(200).json(recipes);
+  }
+  
