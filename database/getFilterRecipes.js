9838fc0fdbbd9cb50d94de8e250120dc7b8c97e5
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-10-26 21:06:23 +0200

    Update getFilterRecipes.js

--- database/getFilterRecipes.js
+++ database/getFilterRecipes.js
@@ -4,9 +4,6 @@ import { client } from "./client"
 export default async function getFilteredRecipes(skip,filter){
 
 
- Database/getFilteredRecipes.js
-
-
     const db = client.db('devdb')
     const results = await db.collection('recipes')
         .find(filter)
