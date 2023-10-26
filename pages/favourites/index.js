5c2a5367d59afee0abf197dc533dab631e957508
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-26 13:43:13 +0200

    Used useEffect to fetch the favourites list from local storage. Added comments.

--- pages/favourites/index.js
+++ pages/favourites/index.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import PreviewList from '@/component/Recipes/Preview/PreviewList';
 const Favourites = () => {
-  const [favouriteRecipesList, setFavouriteRecipesList] = useState(
-    JSON.parse(localStorage.getItem('favouriteRecipesList')) || []
-  );
-
+  const [favouriteRecipesList, setFavouriteRecipesList] = useState([]);
+  //Get the favourites from local storage after the page loads
+  //and change the favouriteRecipesList state.
+  useEffect(() => {
+    const storedData = localStorage.getItem('favouriteRecipesList');
+    if (storedData) {
+      setFavouriteRecipesList(JSON.parse(storedData));
+    }
+  }, []);
   return (
     <>
       <h1>My favourite recipes</h1>
-      <PreviewList recipes={favouriteRecipesList} />;
+      {/* Recipe list component */}
+      <PreviewList recipes={favouriteRecipesList} />
     </>
   );
 };
