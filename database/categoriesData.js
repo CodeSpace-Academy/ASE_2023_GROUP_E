adfcbb1cfd3c1e04128c5e545ccb100f38737ea2
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-10-27 09:34:29 +0200

    merger

--- database/categoriesData.js
+++ database/categoriesData.js
@@ -1,37 +1,17 @@
 import { client } from './client';
 
-<<<<<<< HEAD
-export async function fetchCategoriesFromDatabase() {
-  
- 
-=======
+
 
 export async function fetchCategoriesFromDatabase() {
- 
- 
-
->>>>>>> 29e4f0bc7ad52b8ebbf947520671b513fddf53e0
 
   try {
     await client.connect();
-
-<<<<<<< HEAD
-    const db = client.db('devdb');
-    const categoriesCollection = db.collection('categories');
-    const categories = await categoriesCollection.find().toArray();
-    
-    console.log(categoriesCollection)
-
-=======
 
     const db = client.db('devdb');
     const categoriesCollection = db.collection('categories');
     const categories = await categoriesCollection.find().toArray();
    
     console.log(categoriesCollection)
-
-
->>>>>>> 29e4f0bc7ad52b8ebbf947520671b513fddf53e0
     return {
       categories,
     };
@@ -39,10 +19,6 @@ export async function fetchCategoriesFromDatabase() {
     console.error('Error connecting to the database:', error);
     return {
       categories: [],
-<<<<<<< HEAD
-=======
-
->>>>>>> 29e4f0bc7ad52b8ebbf947520671b513fddf53e0
       error: 'Error connecting to the database',
     };
   } finally {
