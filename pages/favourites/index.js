6118529b0e8bcb1248b95b770acd58c12f08e726
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-30 15:13:04 +0200

    Wrapped favourites content in favourites container.

--- pages/favourites/index.js
+++ pages/favourites/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PreviewList from '@/component/Recipes/Preview/PreviewList';
+import FavouritesContainer from '@/component/FavouritesContainer/FavouritesContainer';
 const Favourites = () => {
   const [favouriteRecipesList, setFavouriteRecipesList] = useState([]);
   //Get the favourites from local storage after the page loads
@@ -12,7 +13,7 @@ const Favourites = () => {
   }, []);
   const favouriteListEmpty = favouriteRecipesList.length === 0;
   return (
-    <>
+    <FavouritesContainer>
       <h1>My favourite recipes</h1>
       {/* Recipe list component */}
       {favouriteListEmpty ? (
@@ -20,7 +21,7 @@ const Favourites = () => {
       ) : (
         <PreviewList recipes={favouriteRecipesList} />
       )}
-    </>
+    </FavouritesContainer>
   );
 };
 
