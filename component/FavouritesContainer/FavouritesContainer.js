fec0d6bfd8a80405fdcdd2b6f44c7182ae07f1f5
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-10-30 15:50:21 +0200

    Moved favourites code to favouritesContainer component

--- component/FavouritesContainer/FavouritesContainer.js
+++ component/FavouritesContainer/FavouritesContainer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import classes from './FavouritesContainer.module.css';
-const FavouritesContainer = ({ children }) => {
-  return <div className={classes.container}>{children}</div>;
+import StateContext from '@/useContext/StateContext';
+import PreviewList from '@/component/Recipes/Preview/PreviewList';
+const FavouritesContainer = () => {
+  const { favouritesList } = StateContext();
+  const favouriteListEmpty = favouritesList.length === 0;
+  return (
+    <div className={classes.container}>
+      <h1>My favourite recipes</h1>
+      {/* Recipe list component */}
+      {favouriteListEmpty ? (
+        <p className={classes.message}>No favourites saved.</p>
+      ) : (
+        <PreviewList recipes={favouritesList} />
+      )}
+    </div>
+  );
 };
 
 export default FavouritesContainer;
--- pages/favourites/index.js
+++ pages/favourites/index.js
@@ -3,19 +3,7 @@ import PreviewList from '@/component/Recipes/Preview/PreviewList';
 import StateContext from '@/useContext/StateContext';
 import FavouritesContainer from '@/component/FavouritesContainer/FavouritesContainer';
 const Favourites = () => {
-  const { favouritesList } = StateContext();
-  const favouriteListEmpty = favouritesList.length === 0;
-  return (
-    <FavouritesContainer>
-      <h1>My favourite recipes</h1>
-      {/* Recipe list component */}
-      {favouriteListEmpty ? (
-        <p>No favourites saved.</p>
-      ) : (
-        <PreviewList recipes={favouritesList} />
-      )}
-    </FavouritesContainer>
-  );
+  return <FavouritesContainer />;
 };
 
 export default Favourites;
