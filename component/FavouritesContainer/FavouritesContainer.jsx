137d864ed528ea6ce850d64b653dc3d20e22cf7f
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-01 19:53:20 +0200

    Added loading spinner

--- component/FavouritesContainer/FavouritesContainer.jsx
+++ component/FavouritesContainer/FavouritesContainer.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import classes from './FavouritesContainer.module.css';
 import StateContext from '../../useContext/StateContext';
 import PreviewList from '../Recipes/Preview/PreviewList';
-
+import LoadingSpinner from '../ui/loadingSpinner/LoadingSpinner';
 export default function FavouritesContainer() {
   const { favouritesList } = StateContext();
   const favouriteListEmpty = favouritesList.length === 0;
+  const favouritesContent = favouriteListEmpty ? (
+    <p className={classes.message}>No favourites saved.</p>
+  ) : (
+    <PreviewList recipes={favouritesList} />
+  );
   return (
     <div className={classes.container}>
       <h1>My favourite recipes</h1>
       {/* Recipe list component */}
-      {favouriteListEmpty ? (
-        <p className={classes.message}>No favourites saved.</p>
-      ) : (
-        <PreviewList recipes={favouritesList} />
-      )}
+      {favouritesList ? favouritesContent : <LoadingSpinner />}
     </div>
   );
 }
