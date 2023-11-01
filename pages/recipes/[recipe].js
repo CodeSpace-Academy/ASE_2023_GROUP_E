5be69979adeb676e1fe8bd2fcc6da048f25dc97e
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-01 22:19:27 +0200

    Can fetch allergens

--- pages/recipes/[recipe].js
+++ pages/recipes/[recipe].js
@@ -3,25 +3,27 @@ import { useRouter } from 'next/router';
 import RecipeDetails from '@/component/Recipes/Details/RecipeDetails';
 
 const RecipePage = () => {
-
-  const router = useRouter()
-  const [ recipe, setRecipe] = useState(null)
-
+  const router = useRouter();
+  const [recipe, setRecipe] = useState(null);
+  const [allergens, setAllergens] = useState([]);
   useEffect(() => {
-
     fetch(`/api/recipes/recipeDetails?filter=${router.query.recipe}`)
-      .then(res => res.json())
-      .then(data => setRecipe(data && data.recipeDetails[0]))
-
-  })
-
+      .then((res) => res.json())
+      .then((data) => setRecipe(data && data.recipeDetails[0]));
+  }, []);
+  useEffect(() => {
+    fetch('/api/recipes/allergens')
+      .then((res) => res.json())
+      .then((data) =>
+        setAllergens(data && data.allergens.allergens[0]?.allergens),
+      );
+    console.log('allergens:', allergens);
+  }, []);
   return (
-  
     <Fragment>
-      {recipe && <RecipeDetails recipe={recipe}/>}
+      {recipe && <RecipeDetails recipe={recipe} allergens={allergens} />}
     </Fragment>
-  
-  )
+  );
 };
 
 export default RecipePage;
