0c96498de23828591667b330ed0536a6f309b5f2
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-01 21:46:07 +0200

    linting

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import '../styles/globals.css';
+import LayoutAll from '../component/Layout/layout';
+import { ContextProvider } from '../useContext/StateContext';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ContextProvider>
+      <LayoutAll>
+        <Component {...pageProps} />
+      </LayoutAll>
+    </ContextProvider>
+  );
+}
