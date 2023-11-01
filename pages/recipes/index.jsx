b4ea7c2819e26f646a591e4d06e35ad96e5fb33a
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-01 21:56:44 +0200

    linting

--- pages/recipes/index.jsx
+++ pages/recipes/index.jsx
@@ -1,44 +1,49 @@
-import PreviewList from '@/component/Recipes/Preview/PreviewList';
 import { useState, useEffect } from 'react';
-import SearchBar from '@/component/filtering/searchCategories/categorySearch';
-import {GrChapterNext} from 'react-icons/gr'
-
+import { GrChapterNext } from 'react-icons/gr';
+import PreviewList from '../../component/Recipes/Preview/PreviewList';
+import SearchBar from '../../component/filtering/searchCategories/categorySearch';
 
 export default function AllRecipes() {
-  const [ results, setResults] = useState(null)
-  const [ sortDate, setSortDate ] = useState('title')
-  const [ sortIn, setSortIn ] = useState(false)
-  const [ addSkip, setAddSkip ] = useState(0)
- 
+  const [results, setResults] = useState(null);
+  const [sortDate, setSortDate] = useState('title');
+  const [sortIn, setSortIn] = useState(false);
+  const [addSkip, setAddSkip] = useState(0);
+
   useEffect(() => {
-    const skipNo = parseInt(localStorage.getItem("skipNo"))
-    setAddSkip(skipNo)
+    const skipNo = parseInt(localStorage.getItem('skipNo'), 10);
+    setAddSkip(skipNo);
     fetch(`/api/recipes/preview?skip=${skipNo && skipNo}&limit=${50}&sort=${sortDate}&sortIn=${sortIn ? -1 : 1}`)
-      .then(res => res.json())
-      .then(data => setResults(data.recipes))
-  }, [sortIn, addSkip, sortDate])
+      .then((res) => res.json())
+      .then((data) => setResults(data.recipes));
+  }, [sortIn, addSkip, sortDate]);
 
   function scrollToTop() {
     window.scrollTo({
       top: 0,
       behavior: 'instant', // Smooth scrolling animation
     });
-  };
+  }
 
   return (
-    <main className='previewMain'>
+    <main className="previewMain">
       <SearchBar />
-      <PreviewList sortDate={() => {
-        setSortDate('published')
-        setSortIn(!sortIn)
-        
-        }} recipes={results}/*  click={loadMoreRecipes}  *//>
+      <PreviewList
+        sortDate={() => {
+          setSortDate('published');
+          setSortIn(!sortIn);
+        }}
+        recipes={results}
+      />
 
-     <GrChapterNext color='light gray' fontSize='24px'  onClick={() => {
-        localStorage.setItem("skipNo", addSkip + 50)
-        scrollToTop()
-        }} disabled={false}/>
-     
+      <GrChapterNext
+        color="light gray"
+        fontSize="24px"
+        onClick={() => {
+          localStorage.setItem('skipNo', addSkip + 50);
+          scrollToTop();
+        }}
+        disabled={false}
+      />
       {/* <button>Page: {pageNumber}</button> */}
     </main>
   );
