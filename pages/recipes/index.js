07f46a3cf6318b668b674bb0cd5212b219ea9e96
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-11-01 14:04:57 +0200

    Added sort buttons
    -Prep time
    -Cook time
    -Date
    
    All can sort by ascending and descending order

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,47 +1,56 @@
-import PreviewList from '@/component/Recipes/Preview/PreviewList';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '@/component/filtering/searchCategories/categorySearch';
-import {MdSkipNext} from 'react-icons/md'
-
+import { MdSkipNext } from 'react-icons/md';
+import PreviewList from '@/component/Recipes/Preview/PreviewList';
 
 export default function AllRecipes() {
-  const [ results, setResults] = useState(null)
-  const [ sortDate, setSortDate ] = useState('title')
-  const [ sortIn, setSortIn ] = useState(false)
-  let addSkip
- 
+  const [results, setResults] = useState(null);
+  const [sortField, setSortField] = useState('title'); // Default sort field
+  const [sortOrder, setSortOrder] = useState('asc'); // Default sort order
+  let addSkip;
+
   useEffect(() => {
+    const skipNo = parseInt(localStorage.getItem('skipNo'));
+    addSkip = skipNo;
 
-
-    const skipNo = parseInt(localStorage.getItem("skipNo"))
-    addSkip = skipNo
-    fetch(`/api/recipes/preview?skip=${skipNo && skipNo}&limit=${50}&sort=${sortDate}&sortIn=${sortIn ? -1 : 1}`)
-      .then(res => res.json())
-      .then(data => setResults(data.recipes))
-  }, [sortIn])
+    fetch(
+      `/api/recipes/preview?skip=${skipNo && skipNo}&limit=${50}&sort=${sortField}&sortIn=${sortOrder === 'asc' ? 1 : -1}`
+    )
+      .then((res) => res.json())
+      .then((data) => setResults(data.recipes));
+  }, [sortField, sortOrder]);
 
   function scrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: 'instant', // Smooth scrolling animation
+      behavior: 'instant',
     });
-  };
+  }
 
   return (
     <main>
       <SearchBar />
-      <PreviewList sortDate={() => {
-        setSortDate('published')
-        setSortIn(!sortIn)
-        
-        }} recipes={results}/*  click={loadMoreRecipes}  *//>
-
-     <MdSkipNext onClick={() => {
-        localStorage.setItem("skipNo", addSkip + 50)
-        scrollToTop()
-        }} disabled={false}/>
-     
-      {/* <button>Page: {pageNumber}</button> */}
+      <div className="sort-dropdown">
+        <label>Sort by:</label>
+        <select value={sortField} onChange={(e) => setSortField(e.target.value)}>
+          
+          <option value="prep">Prep time</option>
+          <option value="cook">Cook time</option>
+          <option value="published">Date</option>
+        </select>
+        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+      <MdSkipNext
+        onClick={() => {
+          localStorage.setItem('skipNo', addSkip + 50);
+          scrollToTop();
+        }}
+        disabled={false}
+      />
+      <PreviewList recipes={results} />
     </main>
   );
 }
