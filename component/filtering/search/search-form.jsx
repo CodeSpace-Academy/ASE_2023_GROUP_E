1a524454f83ce8d74c1c952c5b5f000dcd9d87fb
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-03 06:36:57 +0200

    removed unused file

--- component/filtering/search/search-form.jsx
+++ component/filtering/search/search-form.jsx
@@ -2,7 +2,6 @@ import React, { Fragment, useRef, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { debounce } from 'lodash';
 import PreviewList from '../../Recipes/Preview/PreviewList';
-import { addItem } from '../../../database/addToDatabase';
 import classes from './search-from.module.css';
 
 export default function SearchForm() {
@@ -64,4 +63,30 @@ export default function SearchForm() {
       </div>
     </>
   );
+}
+
+
+/**
+ * Asynchronously adds an item to a specified API endpoint using a POST request.
+ *
+ * @param {string} apiPath - The URL or path of the API endpoint where the item will be added. eg('/api/filename')
+ * @param {Object} item - The item to be added to the API. Should be a JavaScript object.eg({key: value})
+ * @returns {Promise<Object>} A promise that resolves to the response data from the API.
+ * @throws {Error} If the POST request fails or the response status is not OK, an error is thrown with a message.
+ */
+async function addItem(apiPath, item) {
+  const response = await fetch(apiPath, {
+    method: 'POST',
+    body: JSON.stringify(item),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!');
+  }
+  return data;
 }
