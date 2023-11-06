07ccc76e19b5c08a9a34898ac4cd0ba04f236590
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-06 11:13:22 +0200

    Linting

--- pages/recipes/[recipe].jsx
+++ pages/recipes/[recipe].jsx
@@ -1,31 +1,35 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import RecipeDetails from '@/component/Recipes/Details/RecipeDetails';
-import LoadingSpinner from '@/component/ui/loadingSpinner/LoadingSpinner';
-const RecipePage = () => {
+import RecipeDetails from '../../component/Recipes/Details/RecipeDetails';
+
+export default function RecipePage() {
   const router = useRouter();
   const [recipe, setRecipe] = useState(null);
   const [allergens, setAllergens] = useState([]);
   useEffect(() => {
     fetch(`/api/recipes/recipeDetails?filter=${router.query.recipe}`)
-      .then((res) => res.json())
-      .then((data) => setRecipe(data.recipeDetails && data.recipeDetails[0]));
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return setRecipe(data.recipeDetails && data.recipeDetails[0]);
+      });
   });
   useEffect(() => {
     fetch('/api/recipes/allergens')
-      .then((res) => res.json())
-      .then((data) =>
-        setAllergens(data.allergens && data.allergens[0].allergens),
-      );
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return setAllergens(data.allergens && data.allergens[0].allergens);
+      });
   }, []);
 
   return (
-    <Fragment>
+    <div>
       {recipe && (
         <RecipeDetails recipe={recipe && recipe} allergens={allergens} />
       )}
-    </Fragment>
+    </div>
   );
-};
-
-export default RecipePage;
+}
