75de4c0f1db0c63fa62776862c86c006dba1420b
Kelello-7206 <tseripe7@gmail.com>
2023-11-07 09:20:25 +0200

    add div

--- component/filtering/filtering/filterForm.jsx
+++ component/filtering/filtering/filterForm.jsx
@@ -1,21 +1,24 @@
 import Select from 'react-select';
 
-function CustomizedHook({options, handleSelectChange, selectedOptions, filter}) {
-
+function CustomizedHook({
+  options,
+  handleSelectChange,
+  selectedOptions,
+  filter,
+}) {
   return (
-    <div>
+    <div className="previewMain">
       <h4>{filter}</h4>
       <Select
-        options={ options && options.map((option) => ({ value: option, label: option }))}
+        options={
+          options && options.map((option) => ({ value: option, label: option }))
+        }
         isMulti
         value={selectedOptions}
         onChange={handleSelectChange}
       />
-
     </div>
   );
 }
 
 export default CustomizedHook;
-
-
