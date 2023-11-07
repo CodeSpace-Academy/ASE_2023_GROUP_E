1b3c105a23e6e4f273a60fd2b3a95330fa6a6b9a
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-07 23:29:13 +0200

    lint filterbyIngredients.jsx

--- component/filtering/filtering/filterbyIngredients.jsx
+++ component/filtering/filtering/filterbyIngredients.jsx
@@ -1,77 +1,75 @@
-import StateContext from "@/useContext/StateContext";
-import { Fragment, useEffect, useState } from "react";
-import CustomizedHook from "./filterForm";
-import Button from "@/component/Button/button";
+import { useEffect, useState } from 'react';
+import StateContext from '../../../useContext/StateContext';
+import CustomizedHook from './filterForm';
+import {BlueButton} from '../../Button/button';
 
-export default function FilterbyIngredients(){
-
-  const { setFilteredResults, filteredResults, total, setTotal  } = StateContext()
+export default function FilterbyIngredients() {
+  const { setFilteredResults, total, setTotal } = StateContext();
   const [ingredients, setIngredients] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState([]);
-    /**
+  /**
    * control whether the the ingredient will filter all the recipes that includes the selected
    * ingredients or filters ingredients that includes atleast one of the inputs
    */
-    const [andOr, setandOr] = useState(false)
- 
-  useEffect(() =>{
-    fetch('/api/filtering/filterOptions/selectOptions?object=ingredients')
-      .then(res => res.json())
-      .then(data => {
-        if (data) {
+  const [andOr, setandOr] = useState(false);
 
-          /**
+  useEffect(() => {
+    fetch('/api/filtering/filterOptions/selectOptions?object=ingredients')
+      .then((res) => { return res.json(); })
+      .then((data) => {
+        if (data) {
+        /**
            * Fetches the ingredients which is an array objects.
            * maps over this array to abstracts the object keys
            */
           const allIngredients = data.recipes.map((item) => {
-              return Object.keys(item.ingredients)
-          })
+            return Object.keys(item.ingredients);
+          });
           /**
            * {@link allIngredients} is an array of arrays
            * which is then combined
            */
-          const slitIngredients = allIngredients.join().split(',')
+          const slitIngredients = allIngredients.join().split(',');
           /**
            * The combined array is then checked to remove any duplicates
-           * then sent into a state 
+           * then sent into a state
            */
           const uniqueIngredients = [...new Set(slitIngredients)];
           setIngredients(uniqueIngredients);
         }
-      })
-  }, [ingredients])
+      });
+  }, [ingredients]);
 
   const handleSelectChange = (selected) => {
     setSelectedOptions(selected);
   };
 
-  const selected = selectedOptions.map((item) => item.value).join(',')
+  const selected = selectedOptions.map((item) => { return item.value; }).join(',');
 
   useEffect(() => {
-    if(selectedOptions.length > 0){
+    if (selectedOptions.length > 0) {
       fetch(`/api/filtering/filterOptions/filterIngredients?selected=${selected}&andOr=${andOr ? '$or' : '$and'}`)
-      .then(res => res.json())
-      .then(data => {
-        setFilteredResults(data && data.recipes[0])
-        setTotal(total + data && data.recipes[1])
-      })
+        .then((res) => { return res.json(); })
+        .then((data) => {
+          setFilteredResults(data && data.recipes[0]);
+          setTotal(total + data && data.recipes[1]);
+        });
     }
-  }, [selectedOptions, andOr])
+  }, [selectedOptions, andOr]);
 
   return (
-    <Fragment>
-      <Button
-        text='switch'
-        color='warning'
-        click={() => setandOr(!andOr)}
+    <>
+      <BlueButton
+        text="switch"
+        // color="warning"
+        click={() => { setandOr(!andOr); }}
       />
-      <CustomizedHook 
-        options={ingredients} 
-        filter={'Filter Ingredients'}
+      <CustomizedHook
+        options={ingredients}
+        filter="Filter Ingredients"
         handleSelectChange={handleSelectChange}
-        selectedOptions={selectedOptions} 
+        selectedOptions={selectedOptions}
       />
-    </Fragment>
-  )
+    </>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -5,19 +5,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['img.sndimg.com', 'images.pexels.com'], // Updated from remotePatterns to domains
-  },
-  eslint: {
-    dirs: [
-      // components
-      'component/Favourites',
-      'component/FavouritesContainer',
-      'component/ui/loadingSpinner',
-      'components/Recipes/SingleRecipeTags',
-      // pages
-      'pages/recipes',
-      'pages/favourites',
-    ],
-  },
+  }
 };
 
 module.exports = nextConfig;
