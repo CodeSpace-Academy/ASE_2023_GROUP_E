360c24509d6848603c6e2d0dc68074567ac8a92d
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-07 22:22:23 +0200

    misssing env

--- database/client.js
+++ database/client.js
@@ -1,6 +1,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
+if(!uri){
+  throw new console.error('Env not found');
+}
+
 export const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
