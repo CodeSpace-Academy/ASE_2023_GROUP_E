d2107c22d2b867b3c8af2bca9fe264bba80d6e70
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-07 23:59:08 +0200

    lint filterTags

--- pages/api/filtering/filterOptions/filterTags.js
+++ pages/api/filtering/filterOptions/filterTags.js
@@ -1,17 +1,13 @@
-import getFilteredTags from "@/database/getData/getFilteredResults"
+import getFilteredTags from '../../../../database/getData/getFilteredResults';
 
-
-export default async function handler(req, res){
-
-  if(req.method === 'GET'){
-
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
     const selected = req.query.selected ? req.query.selected.split(',') : [];
- 
-    try{
-      const {documents,totalMatchingRecipes} = await getFilteredTags(selected)
-      res.status(200).json({ recipes : [documents, totalMatchingRecipes]})
+    try {
+      const { documents, totalMatchingRecipes } = await getFilteredTags(selected);
+      res.status(200).json({ recipes: [documents, totalMatchingRecipes] });
     } catch (error) {
-      res.status(417).json({ message : error})
+      res.status(417).json({ message: error });
     }
   }
 }
