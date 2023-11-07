abcafa068eed5cb1d60971b8d247021694fb774a
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-07 23:55:12 +0200

    lint getFilteredResults.jsx

--- database/getData/getFilteredResults.jsx
+++ database/getData/getFilteredResults.jsx
@@ -1,39 +1,27 @@
-import { client } from "../client";
+import { client } from '../client';
 
-
-export default async function getFilteredTags(input){
+export default async function getFilteredTags(input) {
   const db = client.db('devdb');
-
-
   const countQuery = { tags: { $all: input } };
   const documents = await db.collection('recipes').find(countQuery).limit(5).toArray();
   const totalMatchingRecipes = await db.collection('recipes').countDocuments(countQuery);
 
-
   return { documents, totalMatchingRecipes };
 }
-
 
 export async function getFilteredIngredients(input, andOr) {
   const db = client.db('devdb');
 
-
-  const filterIngredients = { [andOr]: input.map(key => ({ [`ingredients.${key}`]: { $exists: true } })) };
+  const filterIngredients = { [andOr]: input.map((key) => { return ({ [`ingredients.${key}`]: { $exists: true } }); }) };
   const recipes = await db.collection('recipes').find(filterIngredients).limit(5).toArray();
   const totalMatchingRecipes = await db.collection('recipes').countDocuments(filterIngredients);
-
 
   return { recipes, totalMatchingRecipes };
 }
 
-
 export async function getFilteredObjects(object) {
   const db = client.db('devdb');
-  const results = await db.collection('recipes').find().limit(3).project(object).toArray();
-
-
+  const results = await db.collection('recipes').find().limit(3).project(object)
+    .toArray();
   return results;
 }
-
-
-
