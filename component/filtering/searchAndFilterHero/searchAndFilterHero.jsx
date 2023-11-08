7048153ca428c31eecc29d499492f21c35a6ff5e
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-08 22:20:04 +0200

    Added header and styled hero "form"

--- component/filtering/searchAndFilterHero/searchAndFilterHero.jsx
+++ component/filtering/searchAndFilterHero/searchAndFilterHero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import classes from './searchAndFilterHero.module.css';
 const SearchAndFilterHero = ({ children }) => {
-  return <div className={classes.background}>{children}</div>;
+  return (
+    <div className={classes.background}>
+      <div className={classes.inputsContainer}>
+        <h2 className={classes.heading}>Customize your recipe</h2>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default SearchAndFilterHero;
