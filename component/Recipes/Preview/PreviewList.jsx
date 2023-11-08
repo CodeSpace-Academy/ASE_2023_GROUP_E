69b5bafd48ae0c7ddbda321761c9994e433ff7a9
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-08 23:26:42 +0200

    Removed unused recipe title

--- component/Recipes/Preview/PreviewList.jsx
+++ component/Recipes/Preview/PreviewList.jsx
@@ -61,26 +61,6 @@ export default function PreviewList({ recipes, input, sortDate }) {
                        * if true, no matter the case the text in the input is then highlighted on the title of the search results
                        *
                        */}
-                      {input ? (
-                        <div>
-                          {recipe.title
-                            .split(new RegExp(`(${input})`, 'i'))
-                            .map((title, index) => (
-                              <div
-                                key={index}
-                                style={
-                                  title.toLowerCase() === input.toLowerCase()
-                                    ? { color: 'orange' }
-                                    : {}
-                                }
-                              >
-                                <h3>{title}</h3>
-                              </div>
-                            ))}
-                        </div>
-                      ) : (
-                        <h3 className={style.title}>{recipe.title}</h3>
-                      )}
 
                       <div className={style.recipe}>
                         <div>
