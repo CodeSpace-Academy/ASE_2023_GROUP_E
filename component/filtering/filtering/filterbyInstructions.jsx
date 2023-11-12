ca5720b6cf861816be439bf4dbfc07dd4d7fa569
Kelello-7206 <tseripe7@gmail.com>
2023-11-12 08:49:35 +0200

    added two useEffect

--- component/filtering/filtering/filterbyInstructions.jsx
+++ component/filtering/filtering/filterbyInstructions.jsx
@@ -5,10 +5,35 @@ export default function FilterbyInstructions() {
   const { setFilteredResults, total, setTotal, setSelectedInstructionsOptions, selectedInstructionsOptions } = StateContext();
   const [instructions, setInstructions] = useState([]);
 
+  useEffect(() => {
+    fetch('/api/filtering/filterOptions/selectOptions?object=instructions')
+      .then(res => res.json())
+      .then(data => {
+        if (data) {
+          setInstructions(data.instructions);
+        }
+      })
+  }, []);
 
 
+  useEffect(() => {
+    const selectedValue = selectedInstructionsOptions.length > 0 ? selectedInstructionsOptions[0].value : null;
+    
+    if (selectedValue !== null) {
+      fetch(`/api/filtering/filterOptions/filterInstructions?selected=${selectedValue}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data && data.recipes) {
+            setFilteredResults(data.recipes[0]);
+            setTotal(data.recipes[1] || 0);
+          }
+        });
+    } else {
 
- 
+      setFilteredResults([]);
+      setTotal(0);
+    }
+  }, [selectedInstructionsOptions]);
 
 
   return (
