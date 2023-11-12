041bbc33082b7fa3dbee494cdd030a1a2cb24ea2
Kelello-7206 <tseripe7@gmail.com>
2023-11-12 09:28:14 +0200

    added comments

--- component/filtering/filtering/filterbyInstructions.jsx
+++ component/filtering/filtering/filterbyInstructions.jsx
@@ -11,6 +11,7 @@ export default function FilterbyInstructions() {
 
   const [instructions, setInstructions] = useState([]);
 
+  // Fetch instructions data from the server on component mount
   useEffect(() => {
     fetch('/api/filtering/filterOptions/selectOptions?object=instructions')
       .then((res) => res.json())
@@ -21,31 +22,39 @@ export default function FilterbyInstructions() {
       });
   }, []);
 
+  // Handle input change event
   const handleInputChange = (event) => {
+    // Convert input value to integer
     const selectedValue = parseInt(event.target.value, 10);
+    // Updates selected instructions options
     setSelectedInstructionsOptions([
       { value: selectedValue, label: selectedValue },
     ]);
   };
 
+  // Fetches filtered results when selected instructions change
   useEffect(() => {
+    // Get the selected value
     const selectedValue =
       selectedInstructionsOptions.length > 0
         ? selectedInstructionsOptions[0].value
         : null;
 
+    // Fetches data based on selected value
     if (selectedValue !== null) {
       fetch(
         `/api/filtering/filterOptions/filterInstructions?selected=${selectedValue}`,
       )
         .then((res) => res.json())
         .then((data) => {
+          // Updates state with filtered results
           if (data && data.recipes) {
             setFilteredResults(data.recipes[0]);
             setTotal(data.recipes[1] || 0);
           }
         });
     } else {
+      // If no value selected, reset filtered results
       setFilteredResults([]);
       setTotal(0);
     }
@@ -54,6 +63,7 @@ export default function FilterbyInstructions() {
   return (
     <div>
       <h4>Filter Instructions:</h4>
+      {/* Input for entering the number of instructions */}
       <input
         type="number"
         id="instruction"
