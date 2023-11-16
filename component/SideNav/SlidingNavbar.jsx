8bc040ce710d2b42fe2494c1aeb81b59a7fe982c
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-11-16 14:17:01 +0200

    SlidingNavbar

--- component/SideNav/SlidingNavbar.jsx
+++ component/SideNav/SlidingNavbar.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import {
-  AiOutlineMenu,
-  AiOutlineHome,
-  AiOutlineSetting,
-  AiOutlineUser,
-} from 'react-icons/ai';
-import { MdOutlineFastfood } from 'react-icons/md';
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineHome, AiOutlineHeart, AiOutlineUser, AiOutlineSetting, AiOutlineSearch } from 'react-icons/ai';
 import classes from './sideNav.module.css';
 import StateContext from '@/useContext/StateContext';
 import Link from 'next/link';
-import { IoMdClose } from 'react-icons/io';
+import { IoMdClose } from "react-icons/io";
 
 const ExpandableMenu = () => {
   const { setToggleMenu, toggleMenu } = StateContext();
+  const [isNavBarOpen, setIsNavBarOpen] = useState(false);
+
+  const toggleExpand = () => {
+    setToggleMenu(!toggleMenu);
+    setIsNavBarOpen(!isNavBarOpen);
+  };
 
   const menuOptions = [
-    { link: '/', text: 'Nav bar expanded/closed', icon: toggleMenu ? <IoMdClose /> : <AiOutlineMenu /> },
-    { link: '/', text: 'Home', icon: <AiOutlineHome /> },
-    { link: '/recipes', text: 'All Recipes', icon: <MdOutlineFastfood /> },
-    { link: '/favourites', text: 'Favourites', icon: <AiOutlineUser /> },
-    { link: '/', text: 'User', icon: <AiOutlineUser /> },
-    { link: '/', text: 'Settings', icon: <AiOutlineSetting /> },
+    { icon: <AiOutlineHome />, href: '/', text: 'Home' },
+    { icon: <AiOutlineUser />, href: '/profile', text: 'Profile' },
+    { icon: <AiOutlineHeart />, href: '/favourites', text: 'Favourites' },
+    { icon: <AiOutlineSetting />, href: '/', text: 'Settings' },
   ];
-
-  // Filtering menuOptions based on the specified icons and their order
-  const filteredMenuOptions = menuOptions.filter(
-    option =>
-      option.text === 'Nav bar expanded/closed' ||
-      option.text === 'Home' ||
-      option.text === 'All Recipes' ||
-      option.text === 'Favourites' ||
-      option.text === 'User' ||
-      option.text === 'Settings'
-  );
 
   return (
     <div className={classes.pageContainer}>
       <div className={classes.expandableMenu}>
-        {/* Render the toggle button */}
-        {filteredMenuOptions[0].icon}
+        <div className={`${classes.menuToggle}`} onClick={toggleExpand}>
+          {toggleMenu ?  <IoMdClose /> : <AiOutlineMenu />}
+        </div>
 
         <ul className={`${classes.menuOptions} ${toggleMenu ? classes.expanded : ''}`}>
-          {filteredMenuOptions.slice(1).map((option, index) => (
+          {menuOptions.map((option, index) => (
             <li key={index} onClick={() => setToggleMenu(false)}>
-              <div className={classes.link}>{option.icon}</div>
-              <Link href={option.link}>
-                <p className={classes.optionName}>{option.text}</p>
-              </Link>
+              {toggleMenu ? (
+                <>
+                  <Link href={option.href}>
+                    <div className={classes.link}>{option.icon}</div>
+                    <p className={`${classes.optionName} ${classes.optionNameBold}`}>{option.text}</p>
+                  </Link>
+                </>
+              ) : (
+                <div className={classes.links2}>{option.icon}</div>
+              )}
             </li>
           ))}
         </ul>
+      </div>
+      
+      <div className={classes.mobileMenu}>
+        {menuOptions.map((option, index) => (
+          <p key={index}>{option.icon}</p>
+        ))}
       </div>
     </div>
   );
