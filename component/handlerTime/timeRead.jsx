e6504203f364e0e5553a5cf02ec42a1822de531d
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-16 11:44:41 +0200

    documented numToTime function

--- component/handlerTime/timeRead.jsx
+++ component/handlerTime/timeRead.jsx
@@ -5,16 +5,22 @@ import { LuAlarmClock } from 'react-icons/lu';
 import { BsPerson } from 'react-icons/bs';
 import classes from './timeRead.module.css';
 
-// the function changes number to a time format
+/**
+ * Changes the time from seconds to an hours and minutes format
+ * @param {Number} num Amount of time in seconds
+ * @returns {String} hours : minutes string format
+ */
 
 export default function NumToTime(num) {
   let hours = Math.floor(num / 60);
   let minutes = num % 60;
+  // Puts a zero before the minute value if it is
+  // less than 10
   if (minutes + ''.length < 2) {
     minutes = '0' + minutes;
   }
-  // enable the return to display h for hours and min for minuetes
-  return hours + ':' + minutes;
+  // returns string that seperates the hours and minutes
+  // with a :  return hours + ':' + minutes;
 }
 
 export function PrepandCookTime({ recipe }) {
