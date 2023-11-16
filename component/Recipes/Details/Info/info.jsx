5467b7f115996bd68422a329ff5a557ed4cd4280
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-11-16 20:56:36 +0200

    Update info.jsx
    Added inline comments and JsDoc style comments

--- component/Recipes/Details/Info/info.jsx
+++ component/Recipes/Details/Info/info.jsx
@@ -1,4 +1,5 @@
-/* eslint-disable import/no-useless-path-segments */
+
+// Import React and necessary components/icons/styles
 import { useState } from 'react';
 import { TiEdit } from 'react-icons/ti';
 import classes from './info.module.css';
@@ -10,8 +11,20 @@ import StateContext from '../../../../useContext/StateContext';
 import ErrorMessage from '../../../../component/Error/ErrorMessage';
 import { PrepandCookTime } from '../../../handlerTime/timeRead';
 
+/**
+ * @function Info
+ * @description A component that displays information about a recipe, including category, times, allergens,
+ * description, and options to edit and mark as favorite.
+ * @param {Object} props - The component's properties.
+ * @param {Object} props.recipe - The recipe object containing information.
+ * @param {Array} props.allergens - An array of allergens.
+ * @returns {React.Component} React component displaying recipe information.
+ */
 export default function Info({ recipe, allergens }) {
+  // Access the edit state from the global context
   const { edit, setEdit } = StateContext();
+
+  // State to handle the 'Load more' functionality for the recipe description
   const [loadmore, setLoadmore] = useState(false);
 
   /**
@@ -30,15 +43,19 @@ export default function Info({ recipe, allergens }) {
       }
     });
   });
+
   return (
     <div className={classes.info}>
       <div className={classes.info1}>
+        {/* Display recipe category */}
         <p className={classes.category}>{recipe.category}</p>
 
+        {/* Display preparation and cooking time */}
         <div className={classes.times}>
           <PrepandCookTime recipe={recipe} />
         </div>
 
+        {/* Display allergens, if any */}
         <div className={classes.allergensContainer}>
           <strong>Allergens:</strong>
           {allergenList.length !== 0 ? (
@@ -50,14 +67,17 @@ export default function Info({ recipe, allergens }) {
           )}
         </div>
 
+        {/* Display favorites button */}
         <FavouritesButton recipe={recipe} />
 
+        {/* Display recipe description with 'Load more' functionality */}
         <div className={classes.description}>
           {recipe.description ? (
             <p>
               {loadmore
                 ? recipe.description
                 : recipe.description.substring(0, 180)}
+              {/* Button to toggle 'Load more' */}
               <button
                 type="button"
                 className={classes.loadmore}
@@ -69,16 +89,17 @@ export default function Info({ recipe, allergens }) {
               </button>
             </p>
           ) : (
+            // Display error message if description failed to load
             <ErrorMessage message="Failed to load description." />
           )}
         </div>
 
-        {/**
-         * {@link info} is a props that hold the current descripton that will get modified
-         */}
+        {/* Conditional rendering based on edit state */}
         {edit ? (
+          // Display the edit description component
           <EditDescription info={recipe.description} />
         ) : (
+          // Display the edit button
           <Button
             color="blue"
             text={<TiEdit fontSize="25px" />}
