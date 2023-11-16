09e323298de1de85c08a762d0c7a054e29e92f26
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-11-16 11:16:56 +0200

    updated eslintrc

--- component/SideNav/SlidingNavbar.jsx
+++ component/SideNav/SlidingNavbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { AiOutlineMenu, AiOutlineHome, AiOutlineSetting, AiOutlineUser, AiOutlineHeart, AiOutlineSearch } from 'react-icons/ai';
-import classes from './sideNav.module.css';
-import { MdOutlineFastfood } from 'react-icons/md'
-import StateContext from '@/useContext/StateContext';
+import { MdOutlineFastfood } from 'react-icons/md';
 import Link from 'next/link';
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineSetting,
+  AiOutlineUser,
+  AiOutlineHeart,
+  AiOutlineSearch,
+} from 'react-icons/ai';
+import classes from './sideNav.module.css';
+import StateContext from '@/useContext/StateContext';
 
 const ExpandableMenu = () => {
-  const { setToggleMenu, toggleMenu, setAddSkip } = StateContext()
+  const { setToggleMenu, toggleMenu, setAddSkip } = StateContext();
 
   function skip() {
-    localStorage.setItem("skipNo", 0)
-    setAddSkip(0)
+    localStorage.setItem('skipNo', 0);
+    setAddSkip(0);
   }
 
   const toggleExpand = () => {
@@ -24,12 +31,34 @@ const ExpandableMenu = () => {
   );
 
   const menuOptions = [
-    { icon: Links({ link: '/', text: <AiOutlineHome /> }), name: Links({ link: '/', text: 'Home' }) },
-    { icon: Links({ link: `/recipes-0-_id-asc`, text: <MdOutlineFastfood />, click: skip }), name: Links({ link: `recipes`, text: 'Recipes' }) },
-    { icon: Links({ link: '/favourites', text: <AiOutlineHeart /> }), name: Links({ link: '/favourites', text: 'Favourites' }) },
-    { icon: Links({ link: '/profile', text: <AiOutlineUser /> }), name: Links({ link: '/profile', text: 'Profile' }) },
-    { icon: Links({ link: '/', text: <AiOutlineSetting /> }), name: Links({ link: '/', text: 'Settings' }) },
-    { icon: Links({ link: '/search', text: <AiOutlineSearch /> }), name: Links({ link: '/search', text: 'Search' }) },
+    {
+      icon: Links({ link: '/', text: <AiOutlineHome /> }),
+      name: Links({ link: '/', text: 'Home' }),
+    },
+    {
+      icon: Links({
+        link: `/recipes-0-_id-asc`,
+        text: <MdOutlineFastfood />,
+        click: skip,
+      }),
+      name: Links({ link: `recipes`, text: 'Recipes' }),
+    },
+    {
+      icon: Links({ link: '/favourites', text: <AiOutlineHeart /> }),
+      name: Links({ link: '/favourites', text: 'Favourites' }),
+    },
+    {
+      icon: Links({ link: '/profile', text: <AiOutlineUser /> }),
+      name: Links({ link: '/profile', text: 'Profile' }),
+    },
+    {
+      icon: Links({ link: '/', text: <AiOutlineSetting /> }),
+      name: Links({ link: '/', text: 'Settings' }),
+    },
+    {
+      icon: Links({ link: '/search', text: <AiOutlineSearch /> }),
+      name: Links({ link: '/search', text: 'Search' }),
+    },
   ];
 
   return (
@@ -39,7 +68,11 @@ const ExpandableMenu = () => {
           <AiOutlineMenu />
         </div>
 
-        <ul className={`${classes.menuOptions} ${toggleMenu ? classes.expanded : ''}`}>
+        <ul
+          className={`${classes.menuOptions} ${
+            toggleMenu ? classes.expanded : ''
+          }`}
+        >
           {menuOptions.map((option, index) => (
             <li key={index} onClick={() => setToggleMenu(false)}>
               {toggleMenu ? (
@@ -49,9 +82,7 @@ const ExpandableMenu = () => {
                   <p className={classes.optionName}>{option.name}</p>
                 </>
               ) : (
-                <div className={classes.links2}>
-                  {option.icon}
-                </div>
+                <div className={classes.links2}>{option.icon}</div>
               )}
             </li>
           ))}
