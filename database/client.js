149365b2b37fc7600f716df12b4de93768b6726e
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-16 17:12:12 +0200

    env

--- database/client.js
+++ database/client.js
@@ -1,20 +1,16 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
-export function client(){
-
-  if (!uri) {
-    throw new Error(".env file is missing or has no values");
-  }
-
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-    maxIdleTimeMS: 500,
-  });
-
-  return client
+if (!uri) {
+  throw new Error(".env file is missing or has no values");
 }
+
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  maxIdleTimeMS: 500,
+});
+
