813f87bff294c29555a5e11a291bb9c8d7440ae0
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-19 19:31:58 +0200

    removed unused code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,9 @@
 import HomeWithBackground from '@/component/home-page/HomeWithBackground';
-import { getRecipes } from '@/database/getData/getRecipesData';
-import { useEffect } from 'react';
 
-export default function Home({removeId}) {
-
-  useEffect(() => {
-    console.log(removeId)
-  })
+export default function Home() {
   return (
     <main>
       <HomeWithBackground /> {/* Render the HomeWithBackground component. */}
     </main>
   );
-}
-
-export async function getServerSideProps(){
-
-  const { removeId } = await getRecipes('searchHistory', 0, 5, {createdAt: 1}, [], [], '', 0 , null, '', '', 'bob' )
-  return{
-    props:{
-      removeId
-    }
-  }
 }
