06360028ac8388aa598a228d67033f5c065427b6
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-19 16:40:25 +0200

    categories uses main database module

--- pages/api/filtering/categories.js
+++ pages/api/filtering/categories.js
@@ -1,25 +1,11 @@
-import { getCategories } from "@/database/getData/getCategories";
-
+import { getRecipes } from "@/database/getData/getRecipesData";
 
 export default async function handler(req, res){
 
-
     try{
-        const results = await getCategories()
-        res.status(200).json({ categories: results})
+        const { removeId } = await getRecipes('categories', 0, 5, {createdAt: 1}, [], [], '', 0 , null )
+        res.status(200).json({ categories: removeId})
     }catch(error){
         res.status(417).json({ message: 'failed to load categories'})
     }
 }
-
-// export default async function handler(req, res){
-
-  
-//       try{
-//         const {documents,totalMatchingRecipes} = await getCategories(selected)
-//         res.status(200).json({ recipes : [documents, totalMatchingRecipes]})
-//       } catch (error) {
-//         res.status(417).json({ message: 'failed to load categories'})
-//       }
-//     }
-  
