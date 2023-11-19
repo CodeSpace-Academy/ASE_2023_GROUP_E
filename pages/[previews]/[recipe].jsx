0fda0288f027ae27fb810ba3ff1102e653fe4846
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-19 10:07:58 +0200

    recipe details did not load recipes on reload

--- pages/[previews]/[recipe].jsx
+++ pages/[previews]/[recipe].jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import RecipeDetails from '@/component/Recipes/Details/RecipeDetails';
-import LoadingSpinner from '@/component/ui/loadingSpinner/LoadingSpinner';
+
 const RecipePage = () => {
   const router = useRouter();
   const [recipe, setRecipe] = useState(null);
@@ -11,7 +11,7 @@ const RecipePage = () => {
     fetch(`/api/recipes/recipeDetails?filter=${router.query.recipe}`)
       .then((res) => res.json())
       .then((data) => setRecipe(data.recipeDetails && data.recipeDetails[0]));
-  }, []);
+  }, [recipe]);
 
   useEffect(() => {
     fetch('/api/recipes/allergens')
