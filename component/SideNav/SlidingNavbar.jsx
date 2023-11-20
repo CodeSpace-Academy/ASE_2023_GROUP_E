bae63222b1fc96bcd90df2b347b1402685bd6140
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-11-20 14:05:54 +0200

    Formatted

--- component/SideNav/SlidingNavbar.jsx
+++ component/SideNav/SlidingNavbar.jsx
@@ -1,54 +1,92 @@
 import React from 'react';
-import { AiOutlineMenu,AiOutlineTags, AiOutlineHome, AiOutlineSetting, AiOutlineUser, AiOutlineHeart, AiOutlineSearch } from 'react-icons/ai';
+import {
+  AiOutlineMenu,
+  AiOutlineTags,
+  AiOutlineHome,
+  AiOutlineSetting,
+  AiOutlineUser,
+  AiOutlineHeart,
+  AiOutlineSearch,
+} from 'react-icons/ai';
 import classes from './sideNav.module.css';
-import { MdOutlineFastfood,} from 'react-icons/md'
+import { MdOutlineFastfood } from 'react-icons/md';
 import StateContext from '@/useContext/StateContext';
 import Link from 'next/link';
-import { IoMdClose } from "react-icons/io";
+import { IoMdClose } from 'react-icons/io';
 
-function Links(link, text, click){
+function Links(link, text, click) {
   return (
-    <Link href={link} onClick={click}>{text}</Link>
-  )
+    <Link href={link} onClick={click}>
+      {text}
+    </Link>
+  );
 }
 const ExpandableMenu = () => {
-  const {setToggleMenu, toggleMenu, setAddSkip, selecteTags, selectedIngredients, selectedCategory, selectedInstructionsOptions, andOr} = StateContext()
-  const path = `/recipes-0-id-asc_${selecteTags.map((item) => item.label).join(',')}_${selectedIngredients.map((item) => item.label).join(',')}_${selectedCategory == '' ? selectedCategory : selectedCategory.value}_${selectedInstructionsOptions}_${!andOr}`
-  
-function skip(){
-  localStorage.setItem("skipNo", 0)
-  setAddSkip(0)
-}
+  const {
+    setToggleMenu,
+    toggleMenu,
+    setAddSkip,
+    selecteTags,
+    selectedIngredients,
+    selectedCategory,
+    selectedInstructionsOptions,
+    andOr,
+  } = StateContext();
+  const path = `/recipes-0-id-asc_${selecteTags
+    .map((item) => item.label)
+    .join(',')}_${selectedIngredients.map((item) => item.label).join(',')}_${
+    selectedCategory == '' ? selectedCategory : selectedCategory.value
+  }_${selectedInstructionsOptions}_${!andOr}`;
+
+  function skip() {
+    localStorage.setItem('skipNo', 0);
+    setAddSkip(0);
+  }
   const toggleExpand = () => {
     setToggleMenu(!toggleMenu);
   };
   const menuOptions = [
-    { icon: Links('/',  <AiOutlineHome /> ) , name: Links('/', 'Home')},
-    { icon: Links(path, <MdOutlineFastfood />, skip ), name: Links(path, 'All Recipes') },
-    { icon: Links('/favourites',  <AiOutlineHeart />), name: Links('/favourites', 'Favourites') },
-    { icon: Links(path,  <AiOutlineSearch />), name: Links('/recipes-0-id-asc____0_false', 'Search') },
-    { icon: Links('/profile', <AiOutlineUser />) , name: Links('/profile', 'User') },
-    { icon: Links('/', <AiOutlineSetting />) , name: Links('/', 'Settings')  },
+    { icon: Links('/', <AiOutlineHome />), name: Links('/', 'Home') },
+    {
+      icon: Links(path, <MdOutlineFastfood />, skip),
+      name: Links(path, 'All Recipes'),
+    },
+    {
+      icon: Links('/favourites', <AiOutlineHeart />),
+      name: Links('/favourites', 'Favourites'),
+    },
+    {
+      icon: Links(path, <AiOutlineSearch />),
+      name: Links('/recipes-0-id-asc____0_false', 'Search'),
+    },
+    {
+      icon: Links('/profile', <AiOutlineUser />),
+      name: Links('/profile', 'User'),
+    },
+    { icon: Links('/', <AiOutlineSetting />), name: Links('/', 'Settings') },
   ];
   return (
     <div className={classes.pageContainer}>
       <div className={classes.expandableMenu}>
         <div className={`${classes.menuToggle}`} onClick={toggleExpand}>
-          {toggleMenu ?  <IoMdClose /> : <AiOutlineMenu />}
+          {toggleMenu ? <IoMdClose /> : <AiOutlineMenu />}
         </div>
 
-        <ul className={`${classes.menuOptions} ${toggleMenu ? classes.expanded : ''}`}>
+        <ul
+          className={`${classes.menuOptions} ${
+            toggleMenu ? classes.expanded : ''
+          }`}
+        >
           {menuOptions.map((option, index) => (
             <li key={index} onClick={() => setToggleMenu(false)}>
               {toggleMenu ? (
                 <>
-                  <div className={classes.link}>{option.icon}</div> {/* Increase the font size */}
+                  <div className={classes.link}>{option.icon}</div>{' '}
+                  {/* Increase the font size */}
                   <p className={classes.optionName}>{option.name}</p>
                 </>
               ) : (
-                <div className={classes.links2}>
-                  {option.icon}
-                </div>
+                <div className={classes.links2}>{option.icon}</div>
               )}
             </li>
           ))}
@@ -56,11 +94,9 @@ function skip(){
       </div>
 
       <div className={classes.mobileMenu}>
-          {
-            menuOptions.map((option) => (
-              <p>{option.icon}</p>
-            ))
-          }
+        {menuOptions.map((option) => (
+          <p>{option.icon}</p>
+        ))}
       </div>
     </div>
   );
--- database/addData/addSearchHistory.js
+++ database/addData/addSearchHistory.js
@@ -1,42 +1,40 @@
-import { client } from '../client'
+import { client } from '../client';
 
-
-export default async function addSearchHistory(username, searchHistoryInput){
-  const db = client.db('devdb')
-
+export default async function addSearchHistory(username, searchHistoryInput) {
+  const db = client.db('devdb');
 
   //checks if user has any history
-  const checkUser = await db.collection('searchHistory').findOne({ user: username })
-
+  const checkUser = await db
+    .collection('searchHistory')
+    .findOne({ user: username });
 
   //if user has history, we then push his new search into his history
-  if(checkUser){
+  if (checkUser) {
     //checks users history to not include duplicates
-    const checkhistory = checkUser.input.includes(searchHistoryInput)
+    const checkhistory = checkUser.input.includes(searchHistoryInput);
 
-
-    if(!checkhistory){
-      await db.collection('searchHistory').updateOne({
-        user: username
-      }, {
-        $push:{
-          input: {
-            $each: [searchHistoryInput],
-            $slice: -10
-          }
-        }
-      })
+    if (!checkhistory) {
+      await db.collection('searchHistory').updateOne(
+        {
+          user: username,
+        },
+        {
+          $push: {
+            input: {
+              $each: [searchHistoryInput],
+              $slice: -10,
+            },
+          },
+        },
+      );
     }
 
-
-    return
-  }else{
+    return;
+  } else {
     //if user does not have history, we then create a space where the user's history will be stored
     await db.collection('searchHistory').insertOne({
       user: username,
       input: [searchHistoryInput],
-    })
+    });
   }
-
-
 }
