419d6524b820050d454773532476b354c7c4254f
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-11-20 13:41:05 +0200

    Added font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,15 +2,28 @@
 import '../styles/globals.css';
 import LayoutAll from '@/component/Layout/layout';
 import { ContextProvider } from '../useContext/StateContext';
-import NextNProgress from 'nextjs-progressbar'
+import NextNProgress from 'nextjs-progressbar';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ContextProvider>
-      <LayoutAll>
-        <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow={true}  />
-        <Component {...pageProps} />
-      </LayoutAll>
-    </ContextProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Roboto:ital@1&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <ContextProvider>
+        <LayoutAll>
+          <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow={true} />
+          <Component {...pageProps} />
+        </LayoutAll>
+      </ContextProvider>
+    </>
   );
 }
+
