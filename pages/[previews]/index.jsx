4c94993d76e66c94b3c4bcc619c0d4134cefb05a
Otsile076 <otsilekatane@gmail.com>
2023-11-21 09:46:56 +0200

    Updated alert

--- pages/[previews]/index.jsx
+++ pages/[previews]/index.jsx
@@ -113,13 +113,14 @@ export default function AllRecipes({error, recipes, totalRecipes}) {
           </div>
           <WhiteButton
             click={() => {
-              <h6 style={{color:'white'}}>{totalRecipes == 0 ? alert("No filters have been applied") : ''}</h6>
+              
               setFilteredResults(0)
               setSelectedInstructionsOptions(0)
               setSelectedIngredients([])
               setSelectedTags([])
               setSelectedCategory([])
               setFilteredResults(0)
+              router.query.previews.substring(0, 20) === "recipes-0-id-asc____" ? alert("No filters have been applied") :"";
             }}
             text= 'Clear filters'
           />
