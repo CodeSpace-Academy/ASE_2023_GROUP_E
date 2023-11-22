1453bbbd794d71bfbe1d4a42b1ae78856e5ca32a
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-11-22 14:18:44 +0200

    fixed linting

--- component/filtering/search/search-form.jsx
+++ component/filtering/search/search-form.jsx
@@ -1,8 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { debounce } from 'lodash';
+import { IoCloseCircleSharp } from 'react-icons/io5';
+// eslint-disable-next-line import/no-unresolved
 import classes from './search-from.module.css';
-import { IoCloseCircleSharp } from "react-icons/io5";
+// eslint-disable-next-line import/no-unresolved
 import StateContext from '../../../useContext/StateContext';
 import { WhiteButton } from '../../Button/button';
 
@@ -106,18 +108,22 @@ export default function SearchForm() {
       {displayHistory
         && (
           <div className={classes.searhHistory}>
-                <div className={classes.close}> 
-            <IoCloseCircleSharp 
-            size={25}
-            onClick={() => setDisplayHistory(false)} />
-             </div>
+            <div className={classes.close}>
+              <IoCloseCircleSharp
+                size={25}
+                onClick={() => { return setDisplayHistory(false); }}
+              />
+            </div>
             {
               searchHistory && searchHistory.map((item, index) => {
                 return (
+                  // eslint-disable-next-line max-len
+                  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
                   <li
+                    // eslint-disable-next-line react/no-array-index-key
                     key={index}
                     onClick={() => {
-                      setDisplayHistory(false)
+                      setDisplayHistory(false);
                       historyItemClickHandler(item);
                     }}
                   >
