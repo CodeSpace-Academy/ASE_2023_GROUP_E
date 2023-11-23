1bbf806a5ae7107d421cf6ab410d68cf1cf39ab7
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-11-23 15:25:59 +0200

    Removed the span and just have the string in the li. Moved the tagButton class to the li. Edited the jsdoc

--- component/Recipes/SingleRecipeTags/SingleRecipeTag.jsx
+++ component/Recipes/SingleRecipeTags/SingleRecipeTag.jsx
@@ -4,16 +4,13 @@ import classes from './SingleRecipeTag.module.css';
 
 /**
  * @function SingleRecipeTag
- * @description Displays an individual tag string as a clickable button.
- * @param {Object} props - The component's properties.
+ * @description Displays an individual tag string from a list of recipe tags .
  * @param {string} props.tag - The tag string to be displayed.
- * @returns {React.Component} React component displaying an individual recipe tag.
+ * @returns {React.Component} List item containing a span with the tag string.
  */
 export default function SingleRecipeTag({ tag }) {
   return (
     // List item containing a clickable button with the tag string
-    <li className={classes.tagItem}>
-      <span className={classes.tagButton}>{tag}</span>
-    </li>
+    <li className={`${classes.tagItem} ${classes.tagButton}`}>{tag}</li>
   );
 }
