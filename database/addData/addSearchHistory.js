db920e84bffa7fd09e4dd01e027d43cbe8f1026f
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-27 21:40:41 +0200

    linted

--- database/addData/addSearchHistory.js
+++ database/addData/addSearchHistory.js
@@ -3,14 +3,14 @@ import { client } from '../client';
 export default async function addSearchHistory(username, searchHistoryInput) {
   const db = client.db('devdb');
 
-  //checks if user has any history
+  // checks if user has any history
   const checkUser = await db
     .collection('searchHistory')
     .findOne({ user: username });
 
-  //if user has history, we then push his new search into his history
+  // if user has history, we then push his new search into his history
   if (checkUser) {
-    //checks users history to not include duplicates
+    // checks users history to not include duplicates
     const checkhistory = checkUser.input.includes(searchHistoryInput);
 
     if (!checkhistory) {
@@ -28,10 +28,8 @@ export default async function addSearchHistory(username, searchHistoryInput) {
         },
       );
     }
-
-    return;
   } else {
-    //if user does not have history, we then create a space where the user's history will be stored
+    // if user does not have history, we then create a space where the user's history will be stored
     await db.collection('searchHistory').insertOne({
       user: username,
       input: [searchHistoryInput],
