8bbfa27f7faa423c76b7869d8e850030994a1e8a
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-11-28 11:50:50 +0200

    linting

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,16 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
-import LayoutAll from '@/component/Layout/layout';
-import { ContextProvider } from '../useContext/StateContext';
 import NextNProgress from 'nextjs-progressbar';
-
+import LayoutAll from '../component/Layout/layout';
+import { ContextProvider } from '../useContext/StateContext';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-     <ContextProvider>
-        <LayoutAll>
-          <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow={true} />
-          <Component {...pageProps} />
-        </LayoutAll>
-      </ContextProvider>
-    </>
+    <ContextProvider>
+      <LayoutAll>
+        <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </LayoutAll>
+    </ContextProvider>
   );
 }
-
