ba1edbfd497aa74cd959e75ab1ce73e1e0e5dcf0
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-11-28 09:59:08 +0200

    linting _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,17 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
-import LayoutAll from '@/component/Layout/layout';
-import { ContextProvider } from '../useContext/StateContext';
 import NextNProgress from 'nextjs-progressbar';
+import LayoutAll from '../component/Layout/';
+import { ContextProvider } from '../useContext/StateContext';
 
-
+// Main App component that serves as the entry point for the Next.js application
 export default function App({ Component, pageProps }) {
   return (
-    <>
-     <ContextProvider>
-        <LayoutAll>
-          <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow={true} />
-          <Component {...pageProps} />
-        </LayoutAll>
-      </ContextProvider>
-    </>
+    <ContextProvider>
+      <LayoutAll>
+        <NextNProgress startPosition={0.3} stopDelayMs={200} height={5} showOnShallow />
+        {/* Render the current page component with its props */}
+        <Component {...pageProps} />
+      </LayoutAll>
+    </ContextProvider>
   );
 }
-
