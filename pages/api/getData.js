c9f0f1c083df0d478706e11e0c1f7ecee581e5cd
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-29 20:04:45 +0200

    linted getRecipesData file

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -1,4 +1,4 @@
-import { getRecipes } from "@/database/getData/getRecipesData";
+import getRecipes from "@/database/getData/getRecipesData";
 
 export default async function handler(req, res){
   if (req.method === "GET") {
