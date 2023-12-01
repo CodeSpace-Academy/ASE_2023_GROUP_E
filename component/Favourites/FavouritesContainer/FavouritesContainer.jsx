343acb28bee5fc389773c31330d84cc5ab8598f9
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-12-01 14:39:05 +0200

    Removed spinner import

--- component/Favourites/FavouritesContainer/FavouritesContainer.jsx
+++ component/Favourites/FavouritesContainer/FavouritesContainer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './FavouritesContainer.module.css';
 import StateContext from '../../../useContext/StateContext';
 import PreviewList from '../../Recipes/Preview/PreviewList';
-import LoadingSpinner from '../../ui/loadingSpinner/LoadingSpinner';
 
 export default function FavouritesContainer() {
   const { favouritesList } = StateContext();
