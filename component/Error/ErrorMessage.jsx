7e3fd45f18a9b04c50fd852b44dfc2f55d0571ee
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-12-05 15:09:33 +0200

    documenting

--- component/Error/ErrorMessage.jsx
+++ component/Error/ErrorMessage.jsx
@@ -1,9 +1,10 @@
+/*Description: Error message component to display error messages*/
 import React from 'react';
 import classes from './ErrorMessage.module.css';
 /**
  * Error message component that takes in  an error message and displays it.
- * @param {string} message
- * @returns
+ * @param {string} message - The error message to be displayed
+ * @returns {JSX.Element} - Returns a JSX element representing the error message
  */
 export default function ErrorMessage({ message }) {
   return (
